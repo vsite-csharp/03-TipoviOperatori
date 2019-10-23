964d1948fedcae209f7ed6e3d2136038dbfe6ac4
Julijan Šribar <julijan.sribar@gmail.com>
2019-10-23 20:38:40 +0200

    Add project files.

--- UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
+++ UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vsite.CSharp.TipoviOperatori
+﻿namespace Vsite.CSharp.TipoviOperatori
 {
-    class Program
+    static class Program
     {
         static void Main()
         {
