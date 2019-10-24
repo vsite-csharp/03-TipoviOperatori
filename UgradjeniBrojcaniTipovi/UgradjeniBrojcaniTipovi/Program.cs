3e072a877479cf7e06096a4caac50c6a12711adf
Marko Boroš <Marko.Boros@vsite.hr>
2019-10-24 21:26:17 +0200

    Work with built-in number types

--- UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
+++ UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
@@ -6,26 +6,36 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:030 Ispisati najveći i najmanji broj koji se može pohraniti u tip int.
             Console.WriteLine(int.MaxValue);
-
-            // TODO:031 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
-
-            // TODO:032 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
-
-            // TODO:033 Ispisati granične vrijednosti za tip double.
-
-            // TODO:034 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
-
-            // TODO:035 Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
-
-            // TODO:036 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
-
-            // TODO:037 Dvjema varijablama tipa int pridružiti vrijednost prethodno definirane varijable, odnosno najvećeg broja tipa double i ispisati njihove vrijednosti.
-
-            // TODO:038 Ispisati najveće i najmanje vrijednosti tipa decimal.
-
-            // TODO:039 Varijabli tipa double pridružiti najveću vrijednost tipa double i ispisati rezultat.
+            Console.WriteLine(int.MinValue);
+            int a = int.MaxValue;
+            a++;
+            int b = int.MinValue;
+            b--;
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            double d = double.MinValue;
+            Console.WriteLine(d);
+            d = double.MaxValue;
+            Console.WriteLine(d);
+            Console.WriteLine(d / 0);
+            d++;
+            Console.WriteLine(d / 0);
+            d = 0;
+            Console.WriteLine(d / 0);
+            double c = 5.0;
+            a = (int)c;
+            c = double.MaxValue;
+            a = (int)c;
+            Console.WriteLine(a);
+            decimal d1 = decimal.MaxValue;
+            decimal d2 = decimal.MinValue;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            d =(double) decimal.MaxValue;
+            double f1 = 1.23456789e23;
+            double f2 = 1.23456788e23;
+            Console.WriteLine(f1-f2);
 
             Console.ReadKey();
         }
