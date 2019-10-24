b426c36c0f9d44270a628b934483a821b4673586
Auro Mazzi <auro.mazzi@gmail.com>
2019-10-24 21:02:17 +0200

    Initialize built-in types

--- UgradjeniTipoviInicijalizacija/UgradjeniTipoviInicijalizacija/Program.cs
+++ UgradjeniTipoviInicijalizacija/UgradjeniTipoviInicijalizacija/Program.cs
@@ -6,16 +6,11 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:020 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog)konstruktora te ispisati vrijednost varijable.
-
-            // TODO:021 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
-
-            // TODO:022 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
-
-            // TODO:023 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
-
-            // TODO:024 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
-
+            int a = new int();
+            //int b = new int(4);
+            int b = 4;
+            //string s = new string();
+            string s = "pero";
             Console.ReadKey();
         }
     }
