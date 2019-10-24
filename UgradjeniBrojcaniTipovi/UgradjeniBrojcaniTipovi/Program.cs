d4950f0d3ca730b510ca1debaa7cb6749f85fc07
Marko Vitković <mavitkov@vsite.hr>
2019-10-24 21:14:17 +0200

    done

--- UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
+++ UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
@@ -6,28 +6,46 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:030 Ispisati najveći i najmanji broj koji se može pohraniti u tip int.
+            
             Console.WriteLine(int.MaxValue);
+			Console.WriteLine(int.MinValue);
 
-            // TODO:031 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
+			
+			int a = int.MaxValue;
+			a++;
+			Console.WriteLine(a);
+			
+			int b = int.MaxValue;
+			a--;
+			Console.WriteLine(a);
+			
+			
+			Console.WriteLine(double.MaxValue);
+			
+			Console.WriteLine(double.MaxValue);
+			
+			double c = 3.0;
+			Console.WriteLine(c / 0);
+			double d = -3.0;
+			Console.WriteLine(d / 0);
 
-            // TODO:032 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
+			
+			double f = 0;
+			Console.WriteLine(f / 0);
+			
+			double e = 5;
+			
+			//int g = double.MaxValue;
 
-            // TODO:033 Ispisati granične vrijednosti za tip double.
-
-            // TODO:034 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
-
-            // TODO:035 Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
-
-            // TODO:036 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
-
-            // TODO:037 Dvjema varijablama tipa int pridružiti vrijednost prethodno definirane varijable, odnosno najvećeg broja tipa double i ispisati njihove vrijednosti.
-
-            // TODO:038 Ispisati najveće i najmanje vrijednosti tipa decimal.
-
-            // TODO:039 Varijabli tipa double pridružiti najveću vrijednost tipa double i ispisati rezultat.
-
-            Console.ReadKey();
+			
+			decimal s = decimal.MaxValue;
+			decimal j = decimal.MinValue;
+			Console.WriteLine(s);
+			Console.WriteLine(j);
+			
+			double naj = double.MaxValue;
+			Console.WriteLine(naj);
+			Console.ReadKey();
         }
     }
 }
