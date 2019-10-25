48a669e9f87824617d5849863fd377f46fc4037e
Julijan Šribar <julijan.sribar@gmail.com>
2019-10-25 21:37:16 +0200

    Fix TODO comments

--- UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
+++ UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
@@ -9,15 +9,19 @@ namespace Vsite.Csharp.TipoviOperatori
             // TODO:030 Ispisati najveći i najmanji broj koji se može pohraniti u tip int.
             Console.WriteLine(int.MaxValue);
 
-            // TODO:031 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
+            // TODO:031a Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
 
-            // TODO:032 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
+            // TODO:031b Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
 
-            // TODO:033 Ispisati granične vrijednosti za tip double.
+            // TODO:032 U postavkama projekta uključiti opciju za provjeru numeričkog preljeva. Ponovno pokrenuti program i provjeriti što će se dogoditi.
 
-            // TODO:034 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
+            // TODO:033 Definirati varijablu tipa int te dodati naredbu kojom se ta varijabla dijeli s nulom. Pokrenuti program i provjeriti što će se dogoditi.
 
-            // TODO:035 Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
+            // TODO:034 Ispisati granične vrijednosti za tip double.
+
+            // TODO:035a Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
+
+            // TODO:035b Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
 
             // TODO:036 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
 
--- UgradjeniTipoviInicijalizacija/UgradjeniTipoviInicijalizacija/Program.cs
+++ UgradjeniTipoviInicijalizacija/UgradjeniTipoviInicijalizacija/Program.cs
@@ -6,13 +6,13 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:020 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog)konstruktora te ispisati vrijednost varijable.
+            // TODO:020 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
 
             // TODO:021 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
 
-            // TODO:022 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            // TODO:022 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
 
-            // TODO:023 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            // TODO:023 Provjeriti koji su mogući konstruktori za string.
 
             // TODO:024 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
 
