f214429f3d0034745fa9e1e2e50ea8b05976ae76
Ivan Mihin <ivmihin@vsite.hr>
2019-10-31 20:31:11 +0100

    Resolved TypeOf.

--- OperatorTypeof/OperatorTypeof/Program.cs
+++ OperatorTypeof/OperatorTypeof/Program.cs
@@ -9,13 +9,11 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             Type t = typeof(System.DateTime);
 
-            // TODO:120 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
 
-            // TODO:121 Dodati u petlji i ispis tipa svakog člana.
-            //foreach (MemberInfo mi in t.GetMembers())
-            //{
-            //    Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
-            //}
+            foreach (MemberInfo mi in t.GetMembers())
+            {
+                Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
+            }
 
 
             // TODO:122 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
