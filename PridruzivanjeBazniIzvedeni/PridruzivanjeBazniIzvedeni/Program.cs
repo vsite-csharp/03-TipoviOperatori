40b6b5038a4a40412d95cba23694c0de3df95b49
Marko Boroš <Marko.Boros@vsite.hr>
2019-10-31 20:07:48 +0100

    remove todo comments

--- PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
+++ PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
@@ -22,7 +22,6 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:070 Dodajte pozive svih mogućih metoda nad instancama objekata osoba, student i osobaStudent.
             Osoba osoba = new Osoba();
             osoba.DajOib();
             Student student = new Student();
@@ -33,10 +32,6 @@ namespace Vsite.CSharp.TipoviOperatori
             ((Student)osobaStudent).PoložiIspit("Fizika");
             //((Student)osoba).PoložiIspit("Fizika");
             //Student studentOsoba = new Osoba();
-
-
-            // TODO:071 Deklarirajte objekt tipa Student i pokušajte ga instancirati pozivom konstruktora tipa Osoba. Provjerite što prevoditelj javlja.
-
 
             Console.ReadKey();
         }
