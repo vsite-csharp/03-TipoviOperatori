7113dd82fb8b69374a2a35abd7185d3b095fdc34
Filip Velebit Samodol <fisamodo@vsite.hr>
2019-10-31 19:39:26 +0100

    04

--- NumerickaPromocija/NumerickaPromocija/Program.cs
+++ NumerickaPromocija/NumerickaPromocija/Program.cs
@@ -6,11 +6,9 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent. Dijeljitelj i dijeljenik odabrati tako da dijeljenje bude s ostatkom.
             int a = 5;
             int b = 8;
             Console.WriteLine(a / b);
-            // TODO:041 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
             double c = 3;
             Console.WriteLine(c + a);
             Console.ReadKey();
