f5ed7495a090709ff831a663dcec04799cf2a482
Fran Kalinger <frkaling@vsite.hr>
2019-10-31 19:41:47 +0100

    Resolve numerical promotion

--- NumerickaPromocija/NumerickaPromocija/Program.cs
+++ NumerickaPromocija/NumerickaPromocija/Program.cs
@@ -7,9 +7,12 @@ namespace Vsite.Csharp.TipoviOperatori
         static void Main(string[] args)
         {
             // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent. Dijeljitelj i dijeljenik odabrati tako da dijeljenje bude s ostatkom.
-
+            int x = 5;int y = 10;
+            Console.WriteLine(x / y);
             // TODO:041 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
+            double d = 3.14;
+            Console.WriteLine(x + d);
+            x = (int)(x + d);
             Console.ReadKey();
         }
     }
