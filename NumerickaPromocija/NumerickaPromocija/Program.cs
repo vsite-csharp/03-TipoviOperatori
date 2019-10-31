7382ba87035b85911b67054e081056aeebabc325
Krunoslav Bučar <krbuki@gmail.com>
2019-10-31 19:45:29 +0100

    change numeric

--- NumerickaPromocija/NumerickaPromocija/Program.cs
+++ NumerickaPromocija/NumerickaPromocija/Program.cs
@@ -6,9 +6,13 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent. Dijeljitelj i dijeljenik odabrati tako da dijeljenje bude s ostatkom.
+           
+            int a = 42;
+            int b = 7;
+            Console.WriteLine(42 / 7);
 
-            // TODO:041 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double d = 3.14;
+            
 
             Console.ReadKey();
         }
