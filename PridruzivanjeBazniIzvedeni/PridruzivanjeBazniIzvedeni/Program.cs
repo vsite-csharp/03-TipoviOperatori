ea5ba712b3b13bbdca7765ab82c778e6aa442f5f
Fran Kalinger <frkaling@vsite.hr>
2019-10-31 20:06:50 +0100

    Resolve derived to base assigment

--- PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
+++ PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
@@ -22,16 +22,19 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:070 Dodajte pozive svih mogućih metoda nad instancama objekata osoba, student i osobaStudent.
+            
             Osoba osoba = new Osoba();
+            osoba.DajOib();
 
             Student student = new Student();
-
+            student.DajOib();
+            student.PoložiIspit("PNUM");
             Osoba osobaStudent = new Student();
+            osobaStudent.DajOib();
 
-            // TODO:071 Deklarirajte objekt tipa Student i pokušajte ga instancirati pozivom konstruktora tipa Osoba. Provjerite što prevoditelj javlja.
-
-
+            ((Student)osobaStudent).PoložiIspit("MANA2");
+            // ((Student)osoba).PoložiIspit("MANA2");
+            //Student studentKaoOsoba = new Osoba();
             Console.ReadKey();
         }
     }
