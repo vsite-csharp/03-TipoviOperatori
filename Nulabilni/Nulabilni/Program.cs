990a62001250a4bf03fdb20d1d6b88a7e2a1aea9
Filip Velebit Samodol <fisamodo@vsite.hr>
2019-10-31 19:56:15 +0100

    06

--- Nulabilni/Nulabilni/Program.cs
+++ Nulabilni/Nulabilni/Program.cs
@@ -8,19 +8,19 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             double? nulabilniDuplić = Math.PI;
             Console.WriteLine(nulabilniDuplić);
-            
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
 
-            // TODO:050  Dodati naredbu kojom se vrijednost nulabilnog tipa double postavlja na null referencu te ispisati nakon toga sadržaj varijable.
 
 
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
+            // običniDuplić = null;
 
-            // TODO:051 Pridružiti varijabli tipa double nul-referencu. Provjeriti što će se dogoditi.
-
-            // TODO:052 Definirati nulabilnu varijablu korištenjem generičkog tipa Nullable<T>, postaviti vrijednost na nul-referencu i ispisati sadržaj.
-
-            // TODO:053 Postaviti vrijednost gornje varijable na konačnu vrijednost te ispisati sadržaj.
+            Nullable<int> nulabilniInt = null;
+            Console.WriteLine(nulabilniInt);
+            nulabilniInt = 231;
+            Console.WriteLine(nulabilniInt);
 
 
             Console.ReadKey();
--- var/var/Program.cs
+++ var/var/Program.cs
@@ -12,24 +12,14 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:060 Dodati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
+            var i0 = 55;
+            var d0 = 3.5;
 
-            // TODO:061 Dodati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
+            //i0 = d0;
 
-            // TODO:062 Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
+            int i1 = 55;
 
-
-            // TODO:063 Dodati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
-
-            // TODO:064 Dodati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
-
-            // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
-
-            // TODO:066 Deklarirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
-
+            double d1 = 3.5;
             Console.ReadKey();
         }
     }
