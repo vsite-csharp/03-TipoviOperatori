bf5f66dc5124b129795fd24a964e16ddfc0dedfa
BanovJo <banov.josipa@gmail.com>
2019-10-31 20:17:45 +0100

    Resolve as.

--- OperatorAs/OperatorAs/Program.cs
+++ OperatorAs/OperatorAs/Program.cs
@@ -12,7 +12,6 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:100 Pokrenuti program i provjeriti što će se ispisati te koja će iznimka biti bačena.
             Osoba janko = new Student();
             Student jankoStudent = janko as Student;
             if (jankoStudent != null)
@@ -39,7 +38,6 @@ namespace Vsite.CSharp.TipoviOperatori
             Console.ReadKey();
         }
 
-        // TODO:101 Pogledati donje pretvorbe i razjasniti razliku.
 
         // ovo je česti način pisanja kada želimo napraviti "sigurnu" pretvorbu:
         static void Pretvorba1()
