c09a254668ec37842b0615a9156e9263bad0f184
Domagoj Krmpotić <domagojkrmpoti3@gmail.com>
2019-10-31 19:50:18 +0100

    Resolve nullable types

--- Nulabilni/Nulabilni/Program.cs
+++ Nulabilni/Nulabilni/Program.cs
@@ -9,18 +9,19 @@ namespace Vsite.CSharp.TipoviOperatori
             double? nulabilniDuplić = Math.PI;
             Console.WriteLine(nulabilniDuplić);
 
-            // TODO:050  Dodati naredbu kojom se vrijednost nulabilnog tipa double postavlja na null referencu te ispisati nakon toga sadržaj varijable.
-
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
 
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
 
-            // TODO:051 Pridružiti varijabli tipa double nul-referencu. Provjeriti što će se dogoditi.
+            //običniDuplić = null;
 
-            // TODO:052 Definirati nulabilnu varijablu korištenjem generičkog tipa Nullable<T>, postaviti vrijednost na nul-referencu i ispisati sadržaj.
+            Nullable<int> nulabilniInt = null;
+            Console.WriteLine(nulabilniInt);
 
-            // TODO:053 Postaviti vrijednost gornje varijable na konačnu vrijednost te ispisati sadržaj.
-
+            nulabilniInt = 314;
+            Console.WriteLine(nulabilniInt);
 
             Console.ReadKey();
         }
