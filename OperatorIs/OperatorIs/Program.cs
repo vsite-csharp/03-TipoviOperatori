75af4662017c7c95af4048defb4818b4daa53f18
Marko Vitković <mavitkov@vsite.hr>
2019-10-31 20:12:24 +0100

    done 09

--- OperatorIs/OperatorIs/Program.cs
+++ OperatorIs/OperatorIs/Program.cs
@@ -21,6 +21,10 @@ namespace Vsite.CSharp.TipoviOperatori
 			Console.WriteLine(janko is Osoba);
 			Console.WriteLine(janko is Student);
 
+			Student ivan = new Student();
+			Console.WriteLine(ivan is Osoba);
+			Console.WriteLine(ivan is Student);
+
 			Console.ReadKey();
         }
     }
