726554f500af75bc962d207e26d808f167ff426a
Ivan Blažeka <ivblazek@vsite.hr>
2019-10-31 20:06:55 +0100

    Resolve derived to base assignment

--- PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
+++ PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
@@ -22,15 +22,21 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:070 Dodajte pozive svih mogućih metoda nad instancama objekata osoba, student i osobaStudent.
             Osoba osoba = new Osoba();
+            osoba.DajOib();
 
             Student student = new Student();
+            student.DajOib();
+            student.PoložiIspit("Matematika");
 
             Osoba osobaStudent = new Student();
+            student.DajOib();
+            ((Student)osobaStudent).PoložiIspit("Fizika");
 
-            // TODO:071 Deklarirajte objekt tipa Student i pokušajte ga instancirati pozivom konstruktora tipa Osoba. Provjerite što prevoditelj javlja.
+            //((Student)osoba).PoložiIspit("Fizika");
 
+            //Student studentKaoOsoba = new Osoba();
+            //studentKaoOsoba.PoložiIspit();
 
             Console.ReadKey();
         }
