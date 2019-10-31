559565bde030b50d71cd94b66684eeb1469c9e37
Domagoj Salkić <dosalkic@vsite.hr>
2019-10-31 20:12:48 +0100

    Resolve is

--- OperatorIs/OperatorIs/Program.cs
+++ OperatorIs/OperatorIs/Program.cs
@@ -10,12 +10,18 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:090 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student.
             Osoba osoba = new Osoba();
 
+            Console.WriteLine(osoba is Osoba);
+            Console.WriteLine(osoba is Student);
 
-            // TODO:091 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student.
             Osoba janko = new Student();
+            Console.WriteLine(janko is Osoba);
+            Console.WriteLine(janko is Student);
+
+            Student ivan = new Student();
+            Console.WriteLine(ivan is Osoba);
+            Console.WriteLine(ivan is Student);
 
 
             Console.ReadKey();
