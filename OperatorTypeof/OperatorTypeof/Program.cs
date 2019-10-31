7666d5606dfc521c3706eb2eb954a53d26e4cf89
Danijel Krpes <krpesd@gmail.com>
2019-10-31 20:30:52 +0100

    Resolve typeof operator

--- OperatorTypeof/OperatorTypeof/Program.cs
+++ OperatorTypeof/OperatorTypeof/Program.cs
@@ -11,16 +11,19 @@ namespace Vsite.CSharp.TipoviOperatori
 
             // TODO:120 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
 
+
             // TODO:121 Dodati u petlji i ispis tipa svakog člana.
-            //foreach (MemberInfo mi in t.GetMembers())
-            //{
-            //    Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
-            //}
+            foreach (MemberInfo mi in t.GetMembers())
+            {
+                Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
+            }
 
 
             // TODO:122 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
 
+
             // TODO:123 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+
 
             // TODO:124 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
             foreach (MethodInfo mi in t.GetMethods())
