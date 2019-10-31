240eeec5432eaaf2b50e44638b81e783652212f8
Domagoj Krmpotić <domagojkrmpoti3@gmail.com>
2019-10-31 19:40:31 +0100

    Resolve numerical

--- NumerickaPromocija/NumerickaPromocija/Program.cs
+++ NumerickaPromocija/NumerickaPromocija/Program.cs
@@ -6,10 +6,13 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent. Dijeljitelj i dijeljenik odabrati tako da dijeljenje bude s ostatkom.
+            int a = 5;
+            int b = 10;
+            Console.WriteLine(a/b);
 
-            // TODO:041 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
+            double d = 3.14;
+            Console.WriteLine(a+d);
+            a = (int)(a + d);
             Console.ReadKey();
         }
     }
