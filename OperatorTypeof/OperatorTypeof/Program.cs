5286aabbb99edc23400481fd702369f3954c4129
Domagoj Salkić <dosalkic@vsite.hr>
2019-11-01 16:18:18 +0100

    Resolve type operators

--- OperatorTypeof/OperatorTypeof/Program.cs
+++ OperatorTypeof/OperatorTypeof/Program.cs
@@ -9,26 +9,26 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             Type t = typeof(System.DateTime);
 
-            // TODO:120 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
-            // TODO:121 Dodati u petlji i ispis tipa svakog člana.
             foreach (MemberInfo mi in t.GetMembers())
             {
                 Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
             }
 
 
-            // TODO:122 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
-
-            // TODO:123 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
-
-            // TODO:124 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
             foreach (MethodInfo mi in t.GetMethods())
             {
-                Console.WriteLine($"{mi.ReturnType.ToString()} {mi.Name}");
+                Console.Write($"{mi.Name} {mi.ReturnType.ToString()}   ");
+                if (mi.IsPublic)
+                    Console.WriteLine("public");
+                else if (mi.IsPrivate)
+                    Console.WriteLine("private");
+                else
+                    Console.WriteLine("protected");
                 foreach (var parameter in mi.GetParameters())
                 {
                     Console.WriteLine($"  {parameter.ParameterType.ToString()} {parameter.Name}");
                 }
+                Console.WriteLine();
             }
 
             Console.ReadKey();
