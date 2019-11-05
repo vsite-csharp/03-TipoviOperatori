a898efea62614530fe30898ed0b6ef84bd0a70fd
Auro Mazzi <auro.mazzi@gmail.com>
2019-11-05 22:53:20 +0100

    resolve casting

--- Pretvorba/Pretvorba/Program.cs
+++ Pretvorba/Pretvorba/Program.cs
@@ -31,35 +31,31 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:080 Provjeriti za koja pridruživanja se smije  staviti operator pretvorbe da prevoditelj ne bi prijavio pogrešku a da pri izvođenju ne bude bačena iznimka.
             Osoba osoba = new Osoba();
             Student student = new Student();
             NuklearniFizičar nuklFiz = new NuklearniFizičar();
 
             Osoba osobaStudent = new Student();
             Osoba osobaNuklFiz = new NuklearniFizičar();
-
-            // TODO:081 Provjeriti koja su od donjih pridruživanja dozvoljena.
+            
             osobaStudent = student;
             osobaNuklFiz = nuklFiz;
 
-            // TODO:082 Otkomentirati donje naredbe i provjeriti jesu li dozvoljene.
-            // student = osobaStudent;
-            // nuklFiz = osobaNuklFiz;
+            //student = osobaStudent;
+            //nuklFiz = osobaNuklFiz;
 
-            // TODO:083 Dodati sve moguće pozive metoda za objekt student
+            student.Hodaj();
+            student.PoložiIspit();
 
-
-            // TODO:084 Dodati sve moguće pozive metoda za objekt nuklFiz
-
+            nuklFiz.Hodaj();
+            nuklFiz.NapraviBombu();
 
             Osoba osobaS = student;
-            // TODO:085 Dodati sve moguće pozive metoda za osobaS
+            osobaS.Hodaj();
 
-
-            // TODO:086 Otkomenitari donju naredbu i dodati sve moguće pozive metoda za sos.
-            // Student sos = osobaS;
-
+            //Student sos = osobaS;
+            //sos.Hodaj();
+            //sos.PoložiIspit();
 
             Osoba osobaA = osobaNuklFiz;
             nuklFiz = (NuklearniFizičar)osobaA;
@@ -67,10 +63,9 @@ namespace Vsite.Csharp.TipoviOperatori
             nuklFiz.NapraviBombu();
 
             osobaA = osoba;
-            nuklFiz = (NuklearniFizičar)osobaA;
+            //nuklFiz = (NuklearniFizičar)osobaA;
             nuklFiz.Hodaj();
             nuklFiz.NapraviBombu();
-
 
             Console.ReadKey();
         }
