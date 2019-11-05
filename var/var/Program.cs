014115ccabc159e8a8e4a72fa6303cf478b23e11
Bernard Šetinc <besetinc@vsite.hr>
2019-11-05 14:47:25 +0100

    Intialize var s

--- var/var/Program.cs
+++ var/var/Program.cs
@@ -21,12 +21,13 @@ namespace Vsite.CSharp.TipoviOperatori
             int i1 = 12;
 
             double d1 = 3.14;
-            // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
+            
+            var s = new S();
 
-            // TODO:066 Deklarirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
+            s.a = 3;
+            s.b = 4.16;
+            Console.WriteLine(s.a);
+            Console.WriteLine(s.b);
 
             Console.ReadKey();
         }
