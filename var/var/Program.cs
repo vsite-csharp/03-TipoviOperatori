1378851a1be8ea2d5635056ffaed00f55634771f
Auro Mazzi <auro.mazzi@gmail.com>
2019-11-05 21:25:08 +0100

    Resolve var

--- var/var/Program.cs
+++ var/var/Program.cs
@@ -6,6 +6,7 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         public int a;
         public double b;
+
     }
 
     class Program
@@ -17,12 +18,8 @@ namespace Vsite.CSharp.TipoviOperatori
             //i0 = d0;
             int i1 = 12;
             double d1 = 3.14;
-            // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
-
-            // TODO:066 Deklarirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
+            var s1 = new S();
+            //var s2 = new S(12, 3.14);
 
             Console.ReadKey();
         }
