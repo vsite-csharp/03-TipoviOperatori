0dea6196ff69c0437a23b635cf102d2c8ed1ab0a
Bernard Šetinc <besetinc@vsite.hr>
2019-11-05 15:05:53 +0100

    Resolve method calls on objects

--- Pretvorba/Pretvorba/Program.cs
+++ Pretvorba/Pretvorba/Program.cs
@@ -31,7 +31,6 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:080 Provjeriti za koja pridruživanja se smije  staviti operator pretvorbe da prevoditelj ne bi prijavio pogrešku a da pri izvođenju ne bude bačena iznimka.
             Osoba osoba = new Osoba();
             Student student = new Student();
             NuklearniFizičar nuklFiz = new NuklearniFizičar();
@@ -39,38 +38,43 @@ namespace Vsite.Csharp.TipoviOperatori
             Osoba osobaStudent = new Student();
             Osoba osobaNuklFiz = new NuklearniFizičar();
 
-            // TODO:081 Provjeriti koja su od donjih pridruživanja dozvoljena.
             osobaStudent = student;
             osobaNuklFiz = nuklFiz;
+            
+            //student = osobaStudent;
+            student = (Student)osobaStudent;
+            //nuklFiz = osobaNuklFiz;
+            nuklFiz = (NuklearniFizičar)osobaNuklFiz;
 
-            // TODO:082 Otkomentirati donje naredbe i provjeriti jesu li dozvoljene.
-            // student = osobaStudent;
-            // nuklFiz = osobaNuklFiz;
-
-            // TODO:083 Dodati sve moguće pozive metoda za objekt student
+            student.Hodaj();
+            student.PoložiIspit();
 
 
-            // TODO:084 Dodati sve moguće pozive metoda za objekt nuklFiz
-
+            nuklFiz.Hodaj();
+            nuklFiz.NapraviBombu();
 
             Osoba osobaS = student;
-            // TODO:085 Dodati sve moguće pozive metoda za osobaS
 
+            osobaS.Hodaj();
 
-            // TODO:086 Otkomenitari donju naredbu i dodati sve moguće pozive metoda za sos.
-            // Student sos = osobaS;
+            //Student sos = osobaS;
+            Student sos = (Student)osobaS;
+            sos.Hodaj();
+            sos.PoložiIspit();
+
 
 
             Osoba osobaA = osobaNuklFiz;
+            
             nuklFiz = (NuklearniFizičar)osobaA;
             nuklFiz.Hodaj();
             nuklFiz.NapraviBombu();
-
-            osobaA = osoba;
-            nuklFiz = (NuklearniFizičar)osobaA;
+            
+            osobaA = osoba; 
+            //nuklFiz = (NuklearniFizičar)osobaA;     //baca exception
             nuklFiz.Hodaj();
             nuklFiz.NapraviBombu();
-
+            
 
             Console.ReadKey();
         }
