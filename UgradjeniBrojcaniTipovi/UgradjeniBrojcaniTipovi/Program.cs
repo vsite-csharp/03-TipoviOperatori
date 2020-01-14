11747188cdb8e01450b50c42561cee64282638a0
tleva <tleva@DESKTOP-J8PKM7S>
2020-01-14 20:21:47 +0100

    Resolve ugradeni brojcani tipovi

--- UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
+++ UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
@@ -6,30 +6,37 @@ namespace Vsite.Csharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:030 Ispisati najveći i najmanji broj koji se može pohraniti u tip int.
+    
             Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+   
+            int veliki = int.MaxValue;
+            Console.WriteLine(++veliki);
 
-            // TODO:031a Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
+            int mali = int.MinValue;
+            Console.WriteLine(--mali);
 
-            // TODO:031b Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
 
-            // TODO:032 U postavkama projekta uključiti opciju za provjeru numeričkog preljeva. Ponovno pokrenuti program i provjeriti što će se dogoditi.
+            double a = 3.0;
+            Console.WriteLine(a / 0);
 
-            // TODO:033 Definirati varijablu tipa int te dodati naredbu kojom se ta varijabla dijeli s nulom. Pokrenuti program i provjeriti što će se dogoditi.
+            a = 0.0;
+            Console.WriteLine(a / 0);
 
-            // TODO:034 Ispisati granične vrijednosti za tip double.
+            double b = 5.0;
 
-            // TODO:035a Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
+            int c = (int)b;
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+            double d = (double)decimal.MaxValue;
 
-            // TODO:035b Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
+            double f1 = 1.23456789e23;
+            double f2 = 1.23456788e23;
+            Console.WriteLine(f1 - f2);
+            Console.ReadKey();
 
-            // TODO:036 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
-
-            // TODO:037 Dvjema varijablama tipa int pridružiti vrijednost prethodno definirane varijable, odnosno najvećeg broja tipa double i ispisati njihove vrijednosti.
-
-            // TODO:038 Ispisati najveće i najmanje vrijednosti tipa decimal.
-
-            // TODO:039 Varijabli tipa double pridružiti najveću vrijednost tipa double i ispisati rezultat.
 
             Console.ReadKey();
         }
