7b8be98b57e47d480afffe13bfe3148faedfae38
Jasmin Husić <jhsc1988@gmail.com>
2020-10-29 20:50:18 +0100

    Resolve - numericka promocija

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,31 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            // Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int a = 10;
+            int b = 3;
+            Console.WriteLine(a / b);
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            // Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine((double)a / b);
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            // Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int najveci = int.MaxValue;
+            long dugi = 1;
+            Console.WriteLine(najveci);
+            Console.WriteLine(dugi);
+            Console.WriteLine(najveci + dugi);
 
             // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double c = 3.14;
+            int d = 4;
+            Console.WriteLine(c + d);
 
             // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal dec = 3.14M; // za literale dodamo deciMal
+            Console.WriteLine(dec + (decimal)c); // decimal i double da bi mogli zbrojiti, morali bi se pretvoriti u jaci tip
+            // decimal ima vecu preciznost nego double, a double moze imati vecu vrijednost - nisu kompatibilni
+            // ne moze se implicitno pretvoriti, treba mi eksplicitna konverzija
 
             Console.ReadKey();
         }
