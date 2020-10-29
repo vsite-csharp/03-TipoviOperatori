1dc02ad47601b815f9f8a60ba2fe19ad52c7830d
Ante Žunić <anzunic@vsite.hr>
2020-10-29 20:43:28 +0100

    Resolve Numeric promotion

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,16 +6,28 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            // Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int a = 10;
+            int b = 3;
+            Console.WriteLine(a / b);
+            // Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine((double)a / b);
+            // Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int najveci = int.MaxValue;
+            long dugi = 1;
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine(najveci);
+            Console.WriteLine(dugi);
+            Console.WriteLine(najveci + dugi);
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
-
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
+            // Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double asd = 2.0;
+            int dva = 2;
+            Console.WriteLine(asd + dva);
+            Console.WriteLine(asd / dva);
             // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
-
+            decimal dec = 3.14M;
+            Console.WriteLine(dec + (decimal)asd);
             Console.ReadKey();
         }
     }
