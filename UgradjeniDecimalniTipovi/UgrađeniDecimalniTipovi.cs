4a96d1bcf63d3e52ebb28743be2bf3bd520ef3e6
IvanBukvic <32536551+IvanBukvic@users.noreply.github.com>
2020-10-29 20:25:03 +0100

    Resolve decimal types

--- UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
+++ UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
@@ -10,28 +10,38 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:030 Ispisati granične vrijednosti za tipove double, float i decimal.
+            Console.WriteLine("double.MinValue : {0}", double.MinValue);
+            Console.WriteLine("double.MaxValue : {0}", double.MaxValue);
+            Console.WriteLine("float.MinValue : {0}", float.MinValue);
+            Console.WriteLine("float.MaxValue : {0}", float.MaxValue);
+            Console.WriteLine("decimal.MinValue : {0}", decimal.MinValue);
+            Console.WriteLine("decimal.MaxValue : {0}", decimal.MaxValue);
 
-            // Primjer utjecaja ograničene preciznosti na rezultat oduzimanja/zbrajanja
-            // TODO:031 Definirati tri varijable f1, f2 i f3 tipa float s vrijednostima: 1234567890, 1234567990 i 9.
+            float f1 = 1234567890;
+            float f2 = 1234567990;
+            float f3 = 9;
 
-            // TODO:032 Ispisati razlike (f1-f2), odn. (f1-f3) (pomoću formata "{0:F2}" osigurati ispis svih znamenki) i provjeriti točnost ispisa.
+            Console.WriteLine("{0:F2} - {1:F2} = {2:F2}", f1, f2, f1 - f2);
+            Console.WriteLine("{0:F2} - {1:F2} = {2:F2}", f1, f3, f1 - f3);
+            
+            double trecina1 = 1.0 / 3;
+            double trecina2 = 1 - 2.0 / 3;
 
-            // Primjer binarnog zaokruživanja na rezultat
-            // TODO:033 Definirati dvije varijable trećina1 i trećina2 tipa double s vrijednostima 1/3, odn. (1-2/3). Ispisati njihove vrijednosti te razlike (trećina1-trećina1), odn. (trećina1-tećina2).
+            Console.WriteLine("{0:F20} - {1:F20} = {2:F20}", trecina1, trecina1, trecina1- trecina1);
+            Console.WriteLine("{0:F20} - {1:F20} = {2:F20}", trecina1, trecina2, trecina1 - trecina2);
 
-            // Primjer utjecaja ograničenog raspona na rezultat množenja/dijeljenja
-            // TODO:034 Definirati varijablu tipa double s vrijednošću double.MaxValue te ispisati rezultat njenog množenja s 2. Ponoviti to za double.MinValue.
-
-            // Primjeri neodređenih rezultata dijeljenja
-            // TODO:035 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
-
-            // TODO:036 Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
-
-            // TODO:037 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
-
-            // TODO:038 Varijabli tipa int pridružiti vrijednost prethodno definirane varijable i ispisati njenu vrijednost.
-
+            double max = double.MaxValue;
+            Console.WriteLine(max * 2);
+            double a = 10;
+            a /= 0.0;
+            Console.WriteLine(a);
+            double nula = 0.0;
+            nula /= 0.0;
+            Console.WriteLine(nula);
+            double pet = 5;
+            int c = (int)pet;
+            Console.WriteLine(pet);
+            Console.WriteLine(c);
             Console.ReadKey();
         }
     }
