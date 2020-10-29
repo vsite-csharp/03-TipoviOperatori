4b1467ee140f3d269d4ecc68a5d4534309419dfa
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-29 21:10:40 +0100

    Resolve declaration var

--- var/var/Program.cs
+++ var/var/Program.cs
@@ -12,23 +12,19 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:060 Dodati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
+            var i0 = 10;
 
-            // TODO:061 Dodati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
+            var d0 = 3.14;
+            i0 = (int)d0;
 
-            // TODO:062 Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
+            int i1 = 10;
+            double d1 = 3.14;
 
-
-            // TODO:063 Dodati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
-
-            // TODO:064 Dodati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
-
-            // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
-
-            // TODO:066 Deklarirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
+            var s = new S();
+            s.a = 10;
+            s.b = 2.4;
+            Console.WriteLine(s.a);
+            Console.WriteLine(s.b);
 
             Console.ReadKey();
         }
