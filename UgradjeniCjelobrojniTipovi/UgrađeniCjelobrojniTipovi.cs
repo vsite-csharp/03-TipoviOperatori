8375fa30a5cfa5b046f9930c20c3f6cabb9768ee
Zvonimir Balen <72575824+zvbalen@users.noreply.github.com>
2020-10-29 19:53:20 +0100

    resolve int type

--- UgradjeniCjelobrojniTipovi/UgrađeniCjelobrojniTipovi.cs
+++ UgradjeniCjelobrojniTipovi/UgrađeniCjelobrojniTipovi.cs
@@ -6,29 +6,40 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:020 Otkomentirati donje naredbe koje ispisuju najveće brojeve koji se mogu pohraniti u tip int odn. long.
-            // Console.WriteLine(int.MaxValue);
-            // Console.WriteLine(long.MaxValue);
+            // Otkomentirati donje naredbe koje ispisuju najveće brojeve koji se mogu pohraniti u tip int odn. long.
+             Console.WriteLine(int.MaxValue);
+             Console.WriteLine(long.MaxValue);
 
-            // TODO:021 Dodati naredbe koje će ispisati najmanje brojeve koji se mogu pohraniti u tip int odn. u tip long.
-
+            // Dodati naredbe koje će ispisati najmanje brojeve koji se mogu pohraniti u tip int odn. u tip long.
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MinValue);
             // Primjeri pridruživanja cjelobrojnih varijabli različitih tipova
-            // TODO:022 Deklarirati varijablu tipa int i dodijeliti joj vrijednost 1. 
+            //  Deklarirati varijablu tipa int i dodijeliti joj vrijednost 1. 
+            int a = 1;
 
-            // TODO:023 Deklarirati varijablu tipa long i pridružiti joj vrijednost gornje varijable tipa int.
+            //  Deklarirati varijablu tipa long i pridružiti joj vrijednost gornje varijable tipa int.
+            long dugi = a;
 
-            // TODO:024 Deklarirati varijablu tipa short i pridružiti joj vrijednost gornje varijable tipa int. 
+            //  Deklarirati varijablu tipa short i pridružiti joj vrijednost gornje varijable tipa int. 
+            short kratki = (short)a;
+            //short.MaxValue;
 
             // Primjeri numeričkog preljeva
-            // TODO:025 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
+            //  Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
+            int najveća = int.MaxValue;
+            ++najveća;
+            Console.WriteLine(najveća);
+            //  Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
+            int najmanja = int.MinValue;
+            --najmanja;
+            Console.WriteLine(najmanja);
 
-            // TODO:026 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
 
-            // TODO:027 U postavkama projekta uključiti opciju za provjeru numeričkog preljeva. Ponovno pokrenuti program i provjeriti što će se dogoditi.
+            // U postavkama projekta uključiti opciju za provjeru numeričkog preljeva. Ponovno pokrenuti program i provjeriti što će se dogoditi.
 
             // Primjer cjelobrojnog dijeljenja nulom
-            // TODO:028 Definirati varijablu tipa int te dodati naredbu kojom se ta varijabla dijeli s nulom. Pokrenuti program i provjeriti što će se dogoditi.
-
+            // Definirati varijablu tipa int te dodati naredbu kojom se ta varijabla dijeli s nulom. Pokrenuti program i provjeriti što će se dogoditi.
+            Console.WriteLine(a / 0)
             Console.ReadKey();
         }
     }
