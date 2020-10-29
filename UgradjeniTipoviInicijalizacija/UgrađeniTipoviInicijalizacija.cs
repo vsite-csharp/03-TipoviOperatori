8b69fd2d1e5ab20bf143e3ef93fd8296242f4309
tirupcic <tirupcic@vsite.hr>
2020-10-29 19:32:14 +0100

    resolve built-in type initializations

--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -6,15 +6,18 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            int a = new int();
+            Console.WriteLine(a);
 
-            // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            //int b = new int(4);
+            //Console.WriteLine(b);
 
-            // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            string s = new string('s',10);
+            Console.WriteLine(s);
 
-            // TODO:013 Provjeriti koji su mogući konstruktori za string.
+            string s1 = "pero";
 
-            // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+          
 
             Console.ReadKey();
         }
