308e0910bc6faa98f7439c775a6f763c212594df
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-29 20:39:42 +0100

    resolve numerical promotion

--- NumerickaPromocija/NumerickaPromocija/Program.cs
+++ NumerickaPromocija/NumerickaPromocija/Program.cs
@@ -7,9 +7,21 @@ namespace Vsite.Csharp.TipoviOperatori
         static void Main(string[] args)
         {
             // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent. Dijeljitelj i dijeljenik odabrati tako da dijeljenje bude s ostatkom.
+            int a = 10;
+            int b = 3;
+            Console.WriteLine(a / b);
+            Console.WriteLine((double)a / b);
+            int najveci = int.MaxValue;
+            long dugi = 1;
+            Console.WriteLine(najveci);
+            Console.WriteLine(dugi);
+            Console.WriteLine(najveci + dugi);
 
-            // TODO:041 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
+            double c = 3.14;
+            int d = 4;
+            Console.WriteLine(c + d);
+            decimal dec = 3.14M;
+            Console.WriteLine(dec + (decimal)c);
             Console.ReadKey();
         }
     }
--- UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
+++ UgradjeniBrojcaniTipovi/UgradjeniBrojcaniTipovi/Program.cs
@@ -31,13 +31,10 @@ namespace Vsite.Csharp.TipoviOperatori
 
             double b = 5.0;
 
-            // TODO:037 Dvjema varijablama tipa int pridružiti vrijednost prethodno definirane varijable, odnosno najvećeg broja tipa double i ispisati njihove vrijednosti.
             int c =(int)b;
-            // TODO:038 Ispisati najveće i najmanje vrijednosti tipa decimal.
             Console.WriteLine(decimal.MaxValue);
             Console.WriteLine(decimal.MinValue);
             decimal d1 = decimal.MaxValue;
-            // TODO:039 Varijabli tipa double pridružiti najveću vrijednost tipa double i ispisati rezultat.
             double d = (double)decimal.MaxValue; //mogući gubitak znamenki (mogao bi broj stati, ali doći će do gubitka inf.)-napraviti cast
 
             double f1 = 1.23456789e23F; //double ima veci raspon od floata-(F)
