db3d3e8eea869498cdebb0598d41560a2a36a894
tirupcic <tirupcic@vsite.hr>
2020-10-29 20:58:25 +0100

    Resolve null type

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -11,14 +11,18 @@ namespace Vsite.CSharp.TipoviOperatori
 
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
+            nulabilniDuplić = null;
 
-            // TODO:050  Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
+            Console.WriteLine(nulabilniDuplić);
 
-            // TODO:051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
+            običniDuplić = (double)nulabilniDuplić;
 
-            // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
+            nulabilniDuplić = 10;
+            Console.WriteLine(nulabilniDuplić);
 
-            // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            nulabilniDuplić = običniDuplić;
+            Console.WriteLine(nulabilniDuplić);
+
 
             Console.ReadKey();
         }
