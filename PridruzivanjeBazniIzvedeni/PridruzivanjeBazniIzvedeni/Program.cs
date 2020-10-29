eb99daba164b86254f59fd38e89c46057972fab0
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-29 21:17:03 +0100

    Resolve dynamic declaration

--- PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
+++ PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/Program.cs
@@ -22,14 +22,12 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
-            // TODO:070 Dodajte pozive svih mogućih metoda nad instancama objekata osoba, student i osobaStudent.
             Osoba osoba = new Osoba();
 
             Student student = new Student();
 
             Osoba osobaStudent = new Student();
 
-            // TODO:071 Deklarirajte objekt tipa Student i pokušajte ga instancirati pozivom konstruktora tipa Osoba. Provjerite što prevoditelj javlja.
 
 
             Console.ReadKey();
