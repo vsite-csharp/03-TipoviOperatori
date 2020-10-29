ccf83c533ade568c542ba152cb851e7ceb6ac940
ivana andelic <54909467+ivana474@users.noreply.github.com>
2020-10-29 19:32:20 +0100

    resolve built in types

--- UgradjeniTipoviInicijalizacija/UgradjeniTipoviInicijalizacija/Program.cs
+++ UgradjeniTipoviInicijalizacija/UgradjeniTipoviInicijalizacija/Program.cs
@@ -11,8 +11,7 @@ namespace Vsite.CSharp.TipoviOperatori
             
             //int b = new int(4); //ugrađeni se tipovi mogu inicijalizirati samo pridruživanjem
             int b = 4;
-            
-            
+           
             //string s = new string(); //ni string ne možemo tako inicijalizirati
             string s = "pero";
             // TODO:024 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
