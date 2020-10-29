551c03fb132da04b897da021ca6e78e384f94cb6
Tomislav Peša <72340847+Topesa@users.noreply.github.com>
2020-10-29 19:32:06 +0100

    Resolve buit-in type initializations

--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -6,15 +6,23 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            // Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            int a = new int();
+            Console.WriteLine(a);
 
-            // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            // Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            //int b = new int(4);
+            int b = 4;
 
-            // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            // Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            // string s = new string();
 
-            // TODO:013 Provjeriti koji su mogući konstruktori za string.
+            // Provjeriti koji su mogući konstruktori za string.
+            string s = new string('c', 10);
+            Console.WriteLine(s);
 
-            // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            // Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            string s2 = "Pero";
 
             Console.ReadKey();
         }
