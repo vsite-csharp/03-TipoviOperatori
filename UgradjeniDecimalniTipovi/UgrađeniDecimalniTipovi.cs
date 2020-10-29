6c74eb9bbd484e82d8dfc7eac72da94273ab5047
Zdravko Pašalić <72557820+zdpasali@users.noreply.github.com>
2020-10-29 20:39:44 +0100

    resolve numerical promotion

--- UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
+++ UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
@@ -33,7 +33,7 @@ namespace Vsite.CSharp.TipoviOperatori
 
 
             // Primjer binarnog zaokruživanja na rezultat
-            // TODO:033 Definirati dvije varijable trećina1 i trećina2 tipa double s vrijednostima 1/3, odn. (1-2/3). Ispisati njihove vrijednosti te razlike (trećina1-trećina1), odn. (trećina1-tećina2).
+            //  Definirati dvije varijable trećina1 i trećina2 tipa double s vrijednostima 1/3, odn. (1-2/3). Ispisati njihove vrijednosti te razlike (trećina1-trećina1), odn. (trećina1-tećina2).
             double trećina1 = 1.0 / 3;
             double trećina2 = 1 - 2.0 / 3;
             Console.WriteLine("{0:F20}", trećina1);
