3c9be135ed27ff25746aae31839131f7e383e18c
tirupcic <tirupcic@vsite.hr>
2020-10-29 20:39:36 +0100

    Resolve numerical promotion

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 
 namespace Vsite.CSharp.TipoviOperatori
 {
@@ -6,15 +7,29 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            int a = 10;
+            int b = 3;
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            Console.WriteLine(a / b);
+            Console.WriteLine((double)a / b);
 
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
 
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            int max = int.MaxValue;
+            long l = 1;
+
+            Console.WriteLine(max );
+            Console.WriteLine(l );
+            Console.WriteLine(max + l );
+
+
+            double a1 = 3.14;
+            int b1 = 24;
+            Console.WriteLine(a1 + b1);
+
+            decimal d = 3.14M;
+            Console.WriteLine(d + (decimal)a1);
+
 
             Console.ReadKey();
         }
