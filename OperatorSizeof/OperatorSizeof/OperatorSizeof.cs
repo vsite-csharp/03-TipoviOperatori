5826970aca5a1cc946787a10899145341dacaf58
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2020-11-05 20:32:16 +0100

    Resolve typeof

--- OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
@@ -34,16 +34,24 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
+            // Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
+            Console.WriteLine(sizeof(decimal));
+            // Napisati naredbu kojom će se ispisati duljina podatka tipa double 
+            Console.WriteLine(sizeof(double));
 
-            // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
+            unsafe
+            {
 
-            // TODO:122 Ispisati duljinu tipa MojaStruktura
 
-            // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
+                // Ispisati duljinu tipa MojaStruktura
+                Console.WriteLine(sizeof(MojaStruktura));
+                // Ispisati duljinu tipa MojaSloženaStruktura
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
 
-            // TODO:124 Ispisati duljinu tipa MojaKlasa
+                // Ispisati duljinu tipa MojaKlasa
+                //Console.WriteLine(sizeof(MojaKlasa));
 
+            }
 
             Console.ReadKey();
         }
