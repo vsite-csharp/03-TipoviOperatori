a925049156882ffa0271ca7132bc7a2c3ec661e2
Zvonimir Dražić <zvonimir.drazic@gmail.com>
2020-11-05 22:21:14 +0100

    Resolved Typeof, check output

--- OperatorTypeof/OperatorTypeof/OperatorTypeof.cs
+++ OperatorTypeof/OperatorTypeof/OperatorTypeof.cs
@@ -9,27 +9,43 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             Type t = typeof(System.DateTime);
 
-            // TODO:130 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
+            // ? Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
+                foreach (MemberInfo mi in t.GetMembers()) 
+                Console.WriteLine($" {mi.Name}");
 
-            // TODO:131 Dodati u petlji i ispis tipa svakog člana.
-            //foreach (MemberInfo mi in t.GetMembers())
-            //{
-            //    Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
-            //}
+            // ? Dodati u petlji i ispis tipa svakog člana.
+            foreach (MemberInfo mi in t.GetMembers())
+            {
+                Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
+            }
 
 
-            // TODO:132 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
-
-            // TODO:133 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
-
-            // TODO:134 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
+            // ? Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
             foreach (MethodInfo mi in t.GetMethods())
+                Console.WriteLine($"{mi.Name} {mi.ReturnType.ToString()}");
+
+                // ? Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+
+                // ? Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
+                foreach (MethodInfo mi in t.GetMethods())
             {
                 Console.WriteLine($"{mi.ReturnType.ToString()} {mi.Name}");
+                
                 foreach (var parameter in mi.GetParameters())
                 {
-                    Console.WriteLine($"  {parameter.ParameterType.ToString()} {parameter.Name}");
+                    Console.WriteLine($"  {parameter.ParameterType.ToString()} {parameter.Name} ");
+                    
                 }
+                if (mi.IsPublic == true)
+                    Console.WriteLine($"Public");
+                else if (mi.IsPrivate == true)
+                    Console.WriteLine($"Private");
+                else
+                    Console.WriteLine($"Protected");
+                //ako su sve metode koje vraća  t.GetMethods() public, tada je ok da za sve stoji da su public jer meni tako stoji. 
+                //sve metode u strukturi DateTime za Type t = typeof(System.DateTime); su public 
+                //TODO:140 Profesore da li je takav ispis ispravan?  
+                //Pusham i pullam ako je to to.
             }
 
             Console.ReadKey();
