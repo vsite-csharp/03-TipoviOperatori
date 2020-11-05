558a78d9d300b300709adc82c2cc08429832522a
Vlado Tonković <vlado.tonkovic@outlook.com>
2020-11-05 19:44:43 +0100

    Resolve #1

--- PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
+++ PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
@@ -24,14 +24,26 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             // TODO:080 Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
             Osoba osoba = new Osoba();
+            osoba.DajOib();
+
+            //console.writeline(osoba.gethashcode());
+            //console.writeline(osoba.gettype());
+            //console.writeline(osoba.tostring());
 
             Student student = new Student();
 
+            student.DajOib();
+            student.PoložiIspit("Matematika I");
+
             // TODO:081 Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
             Osoba osobaStudent = new Student();
+            osobaStudent.DajOib();
+            //osobaStudent.PoloziIspit("Fizika");
+
 
             // TODO:082 Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
-
+            //student = new osoba();
+            //student.položiispit("kemija");
 
             Console.ReadKey();
         }
