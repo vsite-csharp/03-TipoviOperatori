7d0cc56973299682b96eb9f9bfbc25d18da48064
Danijel Krpes <krpesd@gmail.com>
2020-11-05 20:08:44 +0100

    Resolve 'is' operator

--- Pretvorba/Pretvorba/Program.cs
+++ Pretvorba/Pretvorba/Program.cs
@@ -43,7 +43,7 @@ namespace Vsite.CSharp.TipoviOperatori
             // Dodati naredbu koja će omogućiti da se na objektu osobaNuklFiz pozove metoda NapraviBombu, pokrenuti program i provjeriti rezultat izvođenja.
             ((NuklearniFizičar)osobaNuklFiz).NapraviBombu();
 
-            // TODO:094 Otkomentirati donje naredbe, provjeriti prijavljuje li program pogreške, pokrenuti program i provjeriti rezultat izvođenja.
+            // Otkomentirati donje naredbe, provjeriti prijavljuje li program pogreške, pokrenuti program i provjeriti rezultat izvođenja.
             Osoba novaOsoba = osobaNuklFiz;
             nuklFizičar = (NuklearniFizičar)novaOsoba;
             nuklFizičar.Hodaj();
