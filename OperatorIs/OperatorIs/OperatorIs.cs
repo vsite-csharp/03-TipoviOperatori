dceec312a47e3aa7868b0eec2bdbe87ca00f86c9
Ante Žunić <anzunic@vsite.hr>
2020-11-05 20:09:51 +0100

    Resolve operator IS

--- OperatorIs/OperatorIs/OperatorIs.cs
+++ OperatorIs/OperatorIs/OperatorIs.cs
@@ -10,13 +10,48 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
+            // Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba osoba = new Osoba();
 
+            if (osoba is Osoba)
+            {
+                Console.WriteLine("osoba je tipa osoba");
+            }
+            else
+            {
+                Console.WriteLine("osoba nije tipa osoba");
+            }
 
-            // TODO:101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
+            if (osoba is Student)
+            {
+                Console.WriteLine("osoba je tipa student"); 
+            }
+            else
+            {
+                Console.WriteLine("osoba nije tipa student");
+            }
+
+
+            // Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba janko = new Student();
 
+            if (janko is Osoba)
+            {
+                Console.WriteLine("janko je tipa osoba");
+            }
+            else
+            {
+                Console.WriteLine("janko nije tipa osoba");
+            }
+
+            if (janko is Student)
+            {
+                Console.WriteLine("janko je tipa student");
+            }
+            else
+            {
+                Console.WriteLine("janko nije tipa student");
+            }
 
             Console.ReadKey();
         }
