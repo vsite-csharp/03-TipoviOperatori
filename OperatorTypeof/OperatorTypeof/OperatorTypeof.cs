c506dd98c28d77306fd2b5d4fdce6b7a1ab44d3c
Paolo Stabile <pastabil@vsite.hr>
2020-11-10 20:15:47 +0100

    Resolve OperatorTypeof

--- OperatorTypeof/OperatorTypeof/OperatorTypeof.cs
+++ OperatorTypeof/OperatorTypeof/OperatorTypeof.cs
@@ -8,21 +8,28 @@ namespace Vsite.CSharp.TipoviOperatori
         static void Main()
         {
             Type t = typeof(System.DateTime);
+            // Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
+            foreach (MemberInfo m in t.GetMembers())
+            {
+                Console.WriteLine(m.Name);
+            }
+            // Dodati u petlji i ispis tipa svakog člana.
+            foreach (MemberInfo mi in t.GetMembers())
+            {
+                Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
+                Console.WriteLine(mi.MemberType);
+            }
+            // Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
+            foreach (MethodInfo m in t.GetMethods())
+            {
+                Console.WriteLine(m.Name, m.MemberType);
+                //  Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+                Console.WriteLine(m.Name);
+                Console.WriteLine(m.ReturnType);
+                // Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
+                Console.WriteLine(m.Attributes);
+            }
 
-            // TODO:130 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
-
-            // TODO:131 Dodati u petlji i ispis tipa svakog člana.
-            //foreach (MemberInfo mi in t.GetMembers())
-            //{
-            //    Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
-            //}
-
-
-            // TODO:132 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
-
-            // TODO:133 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
-
-            // TODO:134 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
             foreach (MethodInfo mi in t.GetMethods())
             {
                 Console.WriteLine($"{mi.ReturnType.ToString()} {mi.Name}");
@@ -31,7 +38,6 @@ namespace Vsite.CSharp.TipoviOperatori
                     Console.WriteLine($"  {parameter.ParameterType.ToString()} {parameter.Name}");
                 }
             }
-
             Console.ReadKey();
         }
     }
