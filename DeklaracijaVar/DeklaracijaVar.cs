45ba7c6c32d0550b28d94f70cca311693ffaac16
Jakov Baleta <Jakov Baleta@DESKTOP-LEG458K>
2021-05-14 21:00:11 +0200

    Resolve declaration var

--- DeklaracijaVar/DeklaracijaVar.cs
+++ DeklaracijaVar/DeklaracijaVar.cs
@@ -12,24 +12,28 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:060 Definirati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
+            // Definirati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
+             var i0 = 10;
+            // Definirati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
+             var d0 = 3.14;
+            // Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
+             i0=(int)d0;
+            // Definirati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
+            int i1 = 10;
+            // Definirati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
+            double d1 = 3.14;
+            // Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
 
-            // TODO:061 Definirati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
+            // Definirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            var s = new S();
+            // Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            s.a = 10;
+            Console.WriteLine(s.a);
 
-            // TODO:062 Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
-
-            // TODO:063 Definirati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
-
-            // TODO:064 Definirati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
-
-            // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
-
-            // TODO:066 Definirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
-            // TODO:068 Napisati naredbu za postavljanje vrijednosti npostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
-
+            s.b = 2.4;
+            Console.WriteLine(s.b);
+            // Napisati naredbu za postavljanje vrijednosti npostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+           // s.c = 3.4;
             Console.ReadKey();
         }
     }
