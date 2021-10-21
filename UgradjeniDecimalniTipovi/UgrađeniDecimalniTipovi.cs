8458f3247bbddcaebcfdaabd63c82032041667b6
Branko Rados <BRados@dspace.hr>
2021-10-21 20:38:22 +0200

    resolve ugradeni cjelobrojni tipovi

--- UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
+++ UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
@@ -10,13 +10,22 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:030 Ispisati granične vrijednosti za tipove double, float i decimal.
-
+            // Ispisati granične vrijednosti za tipove double, float i decimal.
+            Console.WriteLine(Double.MaxValue);
+            Console.WriteLine(Double.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
             // Primjer utjecaja ograničene preciznosti na rezultat oduzimanja/zbrajanja
-            // TODO:031 Definirati tri varijable f1, f2 i f3 tipa float s vrijednostima: 1234567890, 1234567990 i 9.
+            // Definirati tri varijable f1, f2 i f3 tipa float s vrijednostima: 1234567890, 1234567990 i 9.
+            float f1 = 1234567890;
+            float f2 = 1234567990;
+            float f3 = 9;
 
-            // TODO:032 Ispisati razlike (f1-f2), odn. (f1-f3) (pomoću formata "{0:F2}" osigurati ispis svih znamenki) i provjeriti točnost ispisa.
-
+            // Ispisati razlike (f1-f2), odn. (f1-f3) (pomoću formata "{0:F2}" osigurati ispis svih znamenki) i provjeriti točnost ispisa.
+            Console.WriteLine($"f1 - f2 = {0:F2}", (f1 - f2));
+            Console.WriteLine($"f1 - f3 = {0:F2}", (f1 - f3));
             // Primjer binarnog zaokruživanja na rezultat
             // TODO:033 Definirati dvije varijable trećina1 i trećina2 tipa double s vrijednostima 1/3, odn. (1-2/3). Ispisati njihove vrijednosti te razlike (trećina1-trećina1), odn. (trećina1-tećina2).
 
--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -6,15 +6,20 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            // Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            int myInt = new int();
+            Console.WriteLine(">>>>>" + myInt);
 
-            // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            // Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            //int myInt2 = new int(4.);
 
-            // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            //Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            //string myString = new string();
 
-            // TODO:013 Provjeriti koji su mogući konstruktori za string.
-
-            // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            // Provjeriti koji su mogući konstruktori za string.
+            //string myString2 = new string();
+            // Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            string myString3 = "string 1";
 
             Console.ReadKey();
         }
