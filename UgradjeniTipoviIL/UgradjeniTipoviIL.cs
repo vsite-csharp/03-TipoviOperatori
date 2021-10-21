a3f2452befe327163b723e770304db0e618932dc
Ida Midžić <idmidzic@vsite.hr>
2021-10-21 20:38:28 +0200

    Resolve built-in types and for DZ

--- UgradjeniTipoviIL/UgradjeniTipoviIL.cs
+++ UgradjeniTipoviIL/UgradjeniTipoviIL.cs
@@ -14,7 +14,11 @@ namespace Vsite.CSharp.TipoviOperatori
             l = 4294967296;
             float f = 10.3F;
             double dbl = 12.8;
+            // double dbl2 = 1.28;
             decimal dec = 134.45M;
+            // za dz
+            decimal dec2 = 1344.5M;
+            decimal dec3 = 13445M;
             string mirko = "slavko";
         }
     }
