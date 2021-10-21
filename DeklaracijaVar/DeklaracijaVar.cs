5c2a36c7a0ca8a18d622a5b6e28be945b4a64f2d
Deni Kiseljak <dekiseljak@vsite.hr>
2021-10-21 21:09:52 +0200

    Resolve var declaration

--- DeklaracijaVar/DeklaracijaVar.cs
+++ DeklaracijaVar/DeklaracijaVar.cs
@@ -12,24 +12,25 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:060 Definirati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
-
-            // TODO:061 Definirati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
-
-            // TODO:062 Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
-
-            // TODO:063 Definirati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
-
-            // TODO:064 Definirati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
-
+            // 060 Definirati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
+            var i0 = 15;
+            // 061 Definirati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
+            var d0 = 3.45;
+            // 062 Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
+            //i0 = d0;
+            // 063 Definirati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
+            int i1 = 13;
+            // 064 Definirati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
+            double d1 = 3.67;
             // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
 
             // TODO:066 Definirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
-            // TODO:068 Napisati naredbu za postavljanje vrijednosti npostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
-
+            var s = new S();
+            // 067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            s.a = 123;
+            s.b = 1.23;
+            // 068 Napisati naredbu za postavljanje vrijednosti npostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            //s.c = 23;
             Console.ReadKey();
         }
     }
