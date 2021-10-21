7f757a68f098d1aefde76a9cc288f1595469b549
Pavle Pavicic <PavleP@dspace.hr>
2021-10-21 20:38:20 +0200

    Resolve builtin types.

--- UgradjeniTipoviIL/UgradjeniTipoviIL.cs
+++ UgradjeniTipoviIL/UgradjeniTipoviIL.cs
@@ -6,15 +6,18 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:000 Prevesti program te generirani kod pogledati ILDASM-om (IL disassemblerom).
+            // Prevesti program te generirani kod pogledati ILDASM-om (IL disassemblerom).
             // Pogleda li se generirani medjukod (IL), npr. pomocu ILDasm-a vidi se da svi tipovi osim 
             // decimal imaju direktne operacije u medjujeziku!
             int i = 5;
             long l = 10;
             l = 4294967296;
             float f = 10.3F;
-            double dbl = 12.8;
+            double dbl1 = 12.8;
+            double dbl2 = 1.28;
             decimal dec = 134.45M;
+            decimal dec2 = 1344.5M;
+            decimal dec3 = 13445M;
             string mirko = "slavko";
         }
     }
