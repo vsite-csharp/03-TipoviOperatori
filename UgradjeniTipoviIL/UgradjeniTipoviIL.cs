f23187b60bea9cee699b9ce4c259850977ad033f
robilovric <robilovric7@gmail.com>
2021-10-21 21:10:59 +0200

    Resolve var handling

--- UgradjeniTipoviIL/UgradjeniTipoviIL.cs
+++ UgradjeniTipoviIL/UgradjeniTipoviIL.cs
@@ -14,7 +14,7 @@ namespace Vsite.CSharp.TipoviOperatori
             l = 4294967296;
             float f = 10.3F;
             double dbl = 12.8;
-            decimal dec = 134.45M
+            decimal dec = 134.45M;
             decimal dec2 = 1344.5M;
             decimal dec3 = 13445M;
             string mirko = "slavko";
