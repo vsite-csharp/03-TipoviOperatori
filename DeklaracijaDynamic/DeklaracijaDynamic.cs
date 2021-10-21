cfb3a12dd3cb239bd478704e9fc63429cd496277
Ivan Sokolović <isokolovic1@gmail.com>
2021-10-21 21:14:39 +0200

    Resolved dynamic declaration

--- DeklaracijaDynamic/DeklaracijaDynamic.cs
+++ DeklaracijaDynamic/DeklaracijaDynamic.cs
@@ -16,16 +16,19 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            // ok-TODO:070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
             dynamic s = new S();
 
-            // TODO:071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            // ok-TODO:071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
             s.a = 10;
             s.b = 3.14;
 
-            // TODO:072 Dodati naredbe za ispis polja a i b u toj strukturi.
+            // ok-TODO:072 Dodati naredbe za ispis polja a i b u toj strukturi.
+            s.b = 1.23M;
+            s.a = 23338;
 
             // TODO:073 Napisati naredbu za postavljanje vrijednosti npostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            //s.c = "hello";
 
             // TODO:074 Pokrenuti program i provjeriti ispis.
 
