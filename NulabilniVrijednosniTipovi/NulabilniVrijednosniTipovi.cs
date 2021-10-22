6bac44fba9c2e9d8655e524c2f86f5f8b3522c3d
Ognjen Adamović <ognjen.adamovic@billy.hr>
2021-10-22 09:00:28 +0200

    Nulabilni Vrijednosni Tipovi (TODO 50 - 53) Numerička Promocija (TODO 40 - 44) Ugrađeni Decimalni Tipovi (TODO 30 - 38)

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -12,13 +12,18 @@ namespace Vsite.CSharp.TipoviOperatori
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
 
-            // TODO:050  Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
-
-            // TODO:051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
-
-            // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
-
-            // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            // 050  Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
+            // 051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
+            običniDuplić = nulabilniDuplić
+            Console.WriteLine(običniDuplić);
+            // 052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
+            nulabilniDuplić = 321.123;
+            Console.WriteLine(nulabilniDuplić);
+            // 053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            običniDuplić = nulabilniDuplić;
+            Console.WriteLine(običniDuplić);
 
             Console.ReadKey();
         }
--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,24 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
-
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
-
+            // 040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int varijabla1 = 5;
+            int varijabla2 = 4;
+            Console.WriteLine(varijabla1 / varijabla2);
+            // 041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine(varijabla1 / (varijabla1/varijabla2))
             // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
-
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            int varijabla3 = int.MaxValue;
+            long varijabla4 = 1;
+            Console.WriteLine(varijabla3);
+            Console.WriteLine(varijabla4);
+            Console.WriteLine(varijabla4 + varijabla3);
+            // 043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double varijabla5 = 234;
+            Console.WriteLine(varijabla1 + varijabla5);
+            // 044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal varijabla6 = 122.331;
+            Console.WriteLine(varijabla6 + varijabla5);
 
             Console.ReadKey();
         }
