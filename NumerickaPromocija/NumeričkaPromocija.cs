273e06b6186629db35419d39cb0b2ca8e371f08c
Josip Romac <josipromac@gmail.com>
2021-10-23 16:44:08 +0200

    Resolved binary operations od two different types

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,28 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            // Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int prvi = 5;
+            int drugi = 2;
+            Console.WriteLine((prvi / drugi).GetType());
+            // Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine(prvi % drugi);
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            // Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int maxi = int.MaxValue;
+            long dugi = 1;
+            Console.WriteLine(maxi);
+            Console.WriteLine(dugi);
+            Console.WriteLine(maxi+dugi);
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            // Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double nesto = 2D;
+            
+            Console.WriteLine((nesto + prvi).GetType());
 
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            // Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal dec = 3M;
+            //Console.WriteLine((dec + nesto).GetType());
 
             Console.ReadKey();
         }
