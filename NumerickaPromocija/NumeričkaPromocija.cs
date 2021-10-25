1a5add05aabfc1a304ec329e41032fd8b37cef9d
KaTa1112 <katapogacic1@gmail.com>
2021-10-25 22:22:12 +0200

    The task 4

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,30 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            //040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("{0}", (a / b).GetType());//Int
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            //041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine("{0}", (float)a / b);//0.5
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            //042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int c = int.MaxValue;
+            long d = 1;
+            Console.WriteLine("c = {0}", c);// 2147483647
+            Console.WriteLine("d = {0}", d);//1
+            Console.WriteLine("c + d = {0}", c+d);// 2147483648
 
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            //043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double e = 5.5;
+            int f = 5;
+            Console.WriteLine("e+f = {0}", (e+f).GetType());//System.Double
 
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            //044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal g = 10.00M;
+            double h = 5.0;
+            //Console.WriteLine("g+h = {0}", (g+h).GetType());//it is not possible
 
             Console.ReadKey();
         }
