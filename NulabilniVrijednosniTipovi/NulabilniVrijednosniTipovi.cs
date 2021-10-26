72f1f77cb3c598ebb847ddb17349a05932501299
robilovric <robilovric7@gmail.com>
2021-10-26 21:58:56 +0200

    Resolved nullable types

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -12,13 +12,19 @@ namespace Vsite.CSharp.TipoviOperatori
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
 
-            // TODO:050  Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
-
-            // TODO:051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
-
-            // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
-
-            // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            // Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
+            
+            // Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
+            //običniDuplić = nulabilniDuplić;  //cannot inplicity convert double? to double
+            
+            // Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
+            nulabilniDuplić = 77;
+            Console.WriteLine(nulabilniDuplić);
+            
+            // Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            običniDuplić = (double)nulabilniDuplić;  //cannot inplicity convert double? to double
 
             Console.ReadKey();
         }
