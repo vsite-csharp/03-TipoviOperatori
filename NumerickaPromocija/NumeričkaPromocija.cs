d79d41a43603475acee5f3210b0a112ca50e1336
jandela <andjela.cirjak@gmail.com>
2021-10-26 08:46:52 +0200

    Resolve numeric promotion

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,30 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            // Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int var1 = 20;
+            int var2 = 10;
+            int kvocijent = var1 / var2;
+            Console.WriteLine($"Kvocijent dvije varijable tipa int je tipa {kvocijent.GetType()}"); //System.Int32, iznos = 2
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            // Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            var1 = 23;
+            kvocijent = 23 / 10;
+            Console.WriteLine($"Iznos i tip kvocijenta: {kvocijent}, {kvocijent.GetType()}"); //2, System.Int32
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            // Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int intMax = int.MaxValue;
+            long dugi = 1L;
+            Console.WriteLine($"intMax = {intMax}, dugi = {dugi}, intMax + dugi = {intMax+dugi}"); //iznos 2147483648, tip System.Int64
 
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            // Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            int nekiInt = 26;
+            double nekidouble = 4.4;
+            Console.WriteLine($"Zbroj varijabli tipa int i double je tipa {(nekiInt + nekidouble).GetType()}"); //tip System.Double
 
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            // Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal nekiDecimal = 26M;
+            //Console.WriteLine($"Zbroj varijabli tipa decimal i double je tipa {(nekiDecimal + nekidouble).GetType()}"); //varijable tipa decimal i double ne mogu se zbrajati
 
             Console.ReadKey();
         }
