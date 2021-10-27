435c822f22f8ced3c55d1c164a16d1a8edd4a0e8
Leon Pleslic <LPleslic@dspace.hr>
2021-10-27 12:50:40 +0200

    Predavanje i DZ

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,26 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            // 040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int var1 = 2;
+            int var2 = 3;
+            Console.WriteLine((var1 / var2).GetType());
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            // 041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            // 042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int var3 = int.MaxValue;
+            long var4 = 1;
+            Console.WriteLine("int.MaxValue = {0}\nlong 1 = {1}\nint.MaxValue + long 1 = {2}", var3, var4, var3 + var4);
 
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            // 043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double var5 = 4;
+            int var6 = 5;
+            Console.WriteLine((var5 + var6).GetType());
 
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            // 044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal o = 1;
+            //Console.WriteLine((o+f).GetType()); //ne može se zbrojiti
 
             Console.ReadKey();
         }
--- UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
+++ UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
@@ -10,27 +10,45 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:030 Ispisati granične vrijednosti za tipove double, float i decimal.
-
+            // 030 Ispisati granične vrijednosti za tipove double, float i decimal.
+            Console.WriteLine("Double min and max: {0} - {1}", double.MinValue, double.MaxValue);
+            Console.WriteLine("Float min and max: {0} - {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("Decimal min and max: {0} - {1}", decimal.MinValue, decimal.MaxValue);
             // Primjer utjecaja ograničene preciznosti na rezultat oduzimanja/zbrajanja
-            // TODO:031 Definirati tri varijable f1, f2 i f3 tipa float s vrijednostima: 1234567890, 1234567990 i 9.
+            // 031 Definirati tri varijable f1, f2 i f3 tipa float s vrijednostima: 1234567890, 1234567990 i 9.
+            float f1 = 1234567890;
+            float f2 = 1234567990;
+            float f3 = 9;
 
-            // TODO:032 Ispisati razlike (f1-f2), odn. (f1-f3) (pomoću formata "{0:F2}" osigurati ispis svih znamenki) i provjeriti točnost ispisa.
+            // 032 Ispisati razlike (f1-f2), odn. (f1-f3) (pomoću formata "{0:F2}" osigurati ispis svih znamenki) i provjeriti točnost ispisa.
+            Console.WriteLine("(f1-f2) = {0:F2}\n(f1-f3) = {1:F2}", f1 - f2, f1 - f3);
 
             // Primjer binarnog zaokruživanja na rezultat
-            // TODO:033 Definirati dvije varijable trećina1 i trećina2 tipa double s vrijednostima 1/3, odn. (1-2/3). Ispisati njihove vrijednosti te razlike (trećina1-trećina1), odn. (trećina1-tećina2).
+            // 033 Definirati dvije varijable trećina1 i trećina2 tipa double s vrijednostima 1/3, odn. (1-2/3). Ispisati njihove vrijednosti te razlike (trećina1-trećina1), odn. (trećina1-tećina2).
+            double trećina1 = 1 / 3;
+            double trećina2 = (1 - 2 / 3);
+            Console.WriteLine("trećina1 = {0:F2}\ntrećina2 = {1:F2}", trećina1, trećina2);
+            Console.WriteLine("(trećina1 - trećina1) = {0:F2}\n(trećina1 - trećina2) = {1:F}", trećina1 - trećina1, trećina1 - trećina2);
 
             // Primjer utjecaja ograničenog raspona na rezultat množenja/dijeljenja
-            // TODO:034 Definirati varijablu tipa double s vrijednošću double.MaxValue te ispisati rezultat njenog množenja s 2. Ponoviti to za double.MinValue.
+            // 034 Definirati varijablu tipa double s vrijednošću double.MaxValue te ispisati rezultat njenog množenja s 2. Ponoviti to za double.MinValue.
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine("max * 2 = {0}\nDouble min * 2 = {1}", max * 2, min * 2);
 
             // Primjeri neodređenih rezultata dijeljenja
-            // TODO:035 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
+            // 035 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
+            Console.WriteLine(((double)1) / 0);
+            Console.WriteLine(((double)-1) / 0);
 
-            // TODO:036 Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
+            // 036 Provjeriti što će se ispisati kao rezultat kada se varijabla tipa double s vrijednošću 0 podijeli s 0.
+            Console.WriteLine(((double)0) / 0);
 
-            // TODO:037 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
-
-            // TODO:038 Varijabli tipa int pridružiti vrijednost prethodno definirane varijable i ispisati njenu vrijednost.
+            // 037 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
+            double var = 5;
+            // 038 Varijabli tipa int pridružiti vrijednost prethodno definirane varijable i ispisati njenu vrijednost.
+            int var2 = (int)var;
+            Console.WriteLine(var2);
 
             Console.ReadKey();
         }
