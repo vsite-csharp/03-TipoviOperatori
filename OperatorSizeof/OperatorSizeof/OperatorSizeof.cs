6451e59b6431dfa3de385a5da1de88be62751b9d
Ivana Rakarić <ivrakari@vsite.hr>
2021-10-28 20:07:31 +0200

    Operator sizeof

--- OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
@@ -38,11 +38,17 @@ namespace Vsite.CSharp.TipoviOperatori
 
             // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
 
-            // TODO:122 Ispisati duljinu tipa MojaStruktura
+            // :122 Ispisati duljinu tipa MojaStruktura
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaStruktura));
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
 
-            // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
+                //Console.WriteLine(sizeof(MojaKlasa));
+            }
+            // :123 Ispisati duljinu tipa MojaSloženaStruktura
 
-            // TODO:124 Ispisati duljinu tipa MojaKlasa
+            // :124 Ispisati duljinu tipa MojaKlasa
 
 
             Console.ReadKey();
