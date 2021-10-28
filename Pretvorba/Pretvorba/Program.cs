1d3a75aa9d5d732054dca13ab8f205d6e49c7695
Damir Stojanović <DStojanovic@dspace.hr>
2021-10-28 19:52:46 +0200

    Resolve casts base-derived

--- Pretvorba/Pretvorba/Program.cs
+++ Pretvorba/Pretvorba/Program.cs
@@ -27,28 +27,35 @@ namespace Vsite.CSharp.TipoviOperatori
             NuklearniFizičar nuklFizičar = new NuklearniFizičar();
 
             // TODO:090 Dodati pozive svih mogućih metoda za objekte osoba i nuklFizičar, pokrenuti program i provjeriti rezultat izvođenja.
+            osoba.Hodaj();
+            nuklFizičar.Hodaj();
+            nuklFizičar.NapraviBombu();
 
             Osoba osobaNuklFiz = new NuklearniFizičar();
 
-            // TODO:091 Dodati pozive svih mogućih metoda za objekt osobaNuklFiz, pokrenuti program i provjeriti rezultat.
+            // 091 Dodati pozive svih mogućih metoda za objekt osobaNuklFiz, pokrenuti program i provjeriti rezultat.
+            osobaNuklFiz.Hodaj();
+            // Neće proći jer smo ga proglasili osobom: einstein.bum();
 
             // TODO:092 Provjeriti koja su od donjih pridruživanja dozvoljena. Modificirajte naredbe tako da prevoditelj ne prijavljuje pogreške.
-            //osobaNuklFiz = nuklFiz;
-            //nuklFizičar = osobaNuklFiz;
+            osobaNuklFiz = nuklFizičar;
+            // nuklFizičar = osobaNuklFiz;
 
             // TODO:093 Dodati naredbu koja će omogućiti da se na objektu osobaNuklFiz pozove metoda NapraviBombu, pokrenuti program i provjeriti rezultat izvođenja.
-
+            nuklFizičar = (NuklearniFizičar)osobaNuklFiz;
+            nuklFizičar.NapraviBombu();
 
             // TODO:094 Otkomentirati donje naredbe, provjeriti prijavljuje li program pogreške, pokrenuti program i provjeriti rezultat izvođenja.
-            //Osoba novaOsoba = osobaNuklFiz;
-            //nuklFizičar = (NuklearniFizičar)novaOsoba;
-            //nuklFizičar.Hodaj();
-            //nuklFizičar.NapraviBombu();
+            Osoba novaOsoba = osobaNuklFiz;
+            nuklFizičar = (NuklearniFizičar)novaOsoba;
+            nuklFizičar.Hodaj();
+            nuklFizičar.NapraviBombu();
 
-            //novaOsoba = osoba;
-            //nuklFizičar = (NuklearniFizičar)novaOsoba;
-            //nuklFizičar.Hodaj();
-            //nuklFizičar.NapraviBombu();
+            novaOsoba = osoba;
+            // Neće raditi:
+            nuklFizičar = (NuklearniFizičar)novaOsoba;
+            nuklFizičar.Hodaj();
+            nuklFizičar.NapraviBombu();
 
 
             Console.ReadKey();
