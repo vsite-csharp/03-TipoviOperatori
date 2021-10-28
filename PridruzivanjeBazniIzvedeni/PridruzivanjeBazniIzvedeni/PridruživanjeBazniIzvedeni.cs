4159ea2cfce9ed682dfa1b7f97d5ff9a76b567df
Pavle Pavicic <PavleP@dspace.hr>
2021-10-28 19:40:16 +0200

    Resolve base derived assignment.

--- PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
+++ PridruzivanjeBazniIzvedeni/PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
@@ -22,16 +22,18 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:080 Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
+            // Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
             Osoba osoba = new Osoba();
+            osoba.DajOib();
 
             Student student = new Student();
-
-            // TODO:081 Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
+            student.DajOib();
+            student.PoložiIspit("Programiranje u C#-u");
+            // Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
             Osoba osobaStudent = new Student();
-
-            // TODO:082 Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
-
+            osobaStudent.DajOib();
+            // Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
+            //Student studentOsoba = new Osoba();
 
             Console.ReadKey();
         }
