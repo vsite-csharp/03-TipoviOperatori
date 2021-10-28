e1b470adbaee58d5d3617a1fbd34a14008aead4f
Andrea Vrljic <andrea31542@gmail.com>
2021-10-28 20:07:29 +0200

    Resolve sizeOf for structures

--- OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
@@ -38,12 +38,20 @@ namespace Vsite.CSharp.TipoviOperatori
 
             // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
 
-            // TODO:122 Ispisati duljinu tipa MojaStruktura
+            // 122 Ispisati duljinu tipa MojaStruktura
+            unsafe // Properties, Build, check Allow unsafe code 
+            {
+                Console.WriteLine(sizeof(MojaStruktura));  // ispisuje veličinu strukture ovisno o double, integer 128 bit = 16 byte
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
+            }
 
-            // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
+            // 123 Ispisati duljinu tipa MojaSloženaStruktura
 
             // TODO:124 Ispisati duljinu tipa MojaKlasa
-
+            unsafe // Properties, Build, check Allow unsafe code 
+            {
+               //Console.WriteLine(sizeof(MojaKlasa)); //Za referetne tipove ne može odrediti 
+            }
 
             Console.ReadKey();
         }
