6322ebfc6cd7a14314ff81bc30232edc85521a92
Ognjen Adamović <ognjen.adamovic@billy.hr>
2021-10-28 20:18:38 +0200

    Resolved  Operator type of

--- OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
@@ -34,21 +34,21 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
+            // :120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
 
-            // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
+            // :121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
 
             // :122 Ispisati duljinu tipa MojaStruktura
             unsafe
             {
-                Console.WriteLine(sizeof(MojaStruktura);
-                Console.WriteLine(sizeof(MojaSloženaStruktura);
+                    Console.WriteLine(sizeof(MojaStruktura));
+                    Console.WriteLine(sizeof(MojaSloženaStruktura));
 
                 //Console.WriteLine(sizeof(MojaKlasa);
             }
             // :123 Ispisati duljinu tipa MojaSloženaStruktura
 
-            // TODO:124 Ispisati duljinu tipa MojaKlasa
+            // :124 Ispisati duljinu tipa MojaKlasa
 
 
             Console.ReadKey();
