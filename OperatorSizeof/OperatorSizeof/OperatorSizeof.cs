affb1258b451404bcd822d418177b8df573c8cdb
Ivan Kozić <ivkozic@vsite.hr>
2021-10-28 20:03:23 +0200

    todo 130

--- OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
@@ -34,17 +34,26 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
+            // 120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
             Console.WriteLine(sizeof(decimal));
-            // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
+            // 121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
             Console.WriteLine(sizeof(double));
-            // TODO:122 Ispisati duljinu tipa MojaStruktura
-            //Console.WriteLine(sizeof(MojaStruktura));
-            // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
-
-            // TODO:124 Ispisati duljinu tipa MojaKlasa
-
-
+            // 122 Ispisati duljinu tipa MojaStruktura
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaStruktura));
+            }
+            // 123 Ispisati duljinu tipa MojaSloženaStruktura
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
+            }
+            // 124 Ispisati duljinu tipa MojaKlasa
+            //unsafe
+            //{
+            //    Console.WriteLine(sizeof(MojaKlasa));
+            //}
+        
             Console.ReadKey();
         }
     }
