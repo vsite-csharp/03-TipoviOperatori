6dfdc9771e044cfd23f1adf2ec5c4aba9e171bbb
Kata Pogacic <KPogacic@dspace.hr>
2021-10-28 20:19:12 +0200

    Typeof TODO

--- OperatorTypeof/OperatorTypeof/OperatorTypeof.cs
+++ OperatorTypeof/OperatorTypeof/OperatorTypeof.cs
@@ -9,20 +9,20 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             Type t = typeof(System.DateTime);
 
-            // TODO:130 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
+            // 130 Napisati petlju koja će za svaki član kojeg vrati metoda t.GetMembers ispisati ime tog člana.
 
-            // TODO:131 Dodati u petlji i ispis tipa svakog člana.
+            // 131 Dodati u petlji i ispis tipa svakog člana.
             foreach (MemberInfo mi in t.GetMembers())
             {
                 Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
             }
 
 
-            // TODO:132 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
+            // 132 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
 
-            // TODO:133 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+            // 133 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
 
-            // TODO:134 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
+            // 134 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
             foreach (MethodInfo mi in t.GetMethods())
             {
                 Console.WriteLine($"{mi.ReturnType.ToString()} {mi.Name}");
