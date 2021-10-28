3735cc29b2db4f46162456aa226f9eaaf8d5fa87
Kristijan Novak <cobu999@gmail.com>
2021-10-28 19:52:56 +0200

    Resolve cast base

--- Pretvorba/Pretvorba/Program.cs
+++ Pretvorba/Pretvorba/Program.cs
@@ -26,29 +26,32 @@ namespace Vsite.CSharp.TipoviOperatori
             Osoba osoba = new Osoba();
             NuklearniFizičar nuklFizičar = new NuklearniFizičar();
 
-            // TODO:090 Dodati pozive svih mogućih metoda za objekte osoba i nuklFizičar, pokrenuti program i provjeriti rezultat izvođenja.
-
+            //090 Dodati pozive svih mogućih metoda za objekte osoba i nuklFizičar, pokrenuti program i provjeriti rezultat izvođenja.
+            osoba.Hodaj();
+            nuklFizičar.Hodaj();
+            nuklFizičar.NapraviBombu();
             Osoba osobaNuklFiz = new NuklearniFizičar();
+            
 
-            // TODO:091 Dodati pozive svih mogućih metoda za objekt osobaNuklFiz, pokrenuti program i provjeriti rezultat.
-
-            // TODO:092 Provjeriti koja su od donjih pridruživanja dozvoljena. Modificirajte naredbe tako da prevoditelj ne prijavljuje pogreške.
-            //osobaNuklFiz = nuklFiz;
+            //091 Dodati pozive svih mogućih metoda za objekt osobaNuklFiz, pokrenuti program i provjeriti rezultat.
+            osobaNuklFiz.Hodaj();
+            //092 Provjeriti koja su od donjih pridruživanja dozvoljena. Modificirajte naredbe tako da prevoditelj ne prijavljuje pogreške.
+            osobaNuklFiz = nuklFiz;
             //nuklFizičar = osobaNuklFiz;
 
-            // TODO:093 Dodati naredbu koja će omogućiti da se na objektu osobaNuklFiz pozove metoda NapraviBombu, pokrenuti program i provjeriti rezultat izvođenja.
-
-
-            // TODO:094 Otkomentirati donje naredbe, provjeriti prijavljuje li program pogreške, pokrenuti program i provjeriti rezultat izvođenja.
-            //Osoba novaOsoba = osobaNuklFiz;
-            //nuklFizičar = (NuklearniFizičar)novaOsoba;
-            //nuklFizičar.Hodaj();
-            //nuklFizičar.NapraviBombu();
-
-            //novaOsoba = osoba;
-            //nuklFizičar = (NuklearniFizičar)novaOsoba;
-            //nuklFizičar.Hodaj();
-            //nuklFizičar.NapraviBombu();
+            //093 Dodati naredbu koja će omogućiti da se na objektu osobaNuklFiz pozove metoda NapraviBombu, pokrenuti program i provjeriti rezultat izvođenja.
+            nuklFizičar = (NuklearniFizičar)osobaNuklFiz;
+            nuklFizičar.NapraviBombu();
+            //094 Otkomentirati donje naredbe, provjeriti prijavljuje li program pogreške, pokrenuti program i provjeriti rezultat izvođenja.
+            Osoba novaosoba = osobanuklfiz;
+            Nuklfizičar = (NuklearniFizičar)novaosoba;
+            nuklFizičar.hodaj();
+            NuklFizičar.napravibombu();
+            // ovo nbu dobro završilo
+            novaosoba = osoba;
+            nuklFizičar = (NuklearniFizičar)novaosoba;
+            nuklFizičar.Hodaj();
+            nuklFizičar.napravibombu();
 
 
             Console.ReadKey();
