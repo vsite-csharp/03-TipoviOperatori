074482dfd22193cfc22c35ead2c6f93ac420af27
Marina Letica <MLetica@dspace.hr>
2021-10-28 20:07:20 +0200

    Resolved sizeof

--- OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof/OperatorSizeof.cs
@@ -38,9 +38,14 @@ namespace Vsite.CSharp.TipoviOperatori
 
             // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
 
-            // TODO:122 Ispisati duljinu tipa MojaStruktura
+            // :122 Ispisati duljinu tipa MojaStruktura
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaStruktura));
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
+            }
 
-            // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
+            // :123 Ispisati duljinu tipa MojaSloženaStruktura
 
             // TODO:124 Ispisati duljinu tipa MojaKlasa
 
