9df9fdac08e0c0c04d57ad87f2604d4d9d5fc9f4
FranKalinger0998 <f.kalinger@gmail.com>
2022-10-29 13:01:14 +0200

    Var declaration

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -12,13 +12,17 @@ namespace Vsite.CSharp.TipoviOperatori
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
 
-            // TODO:050  Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
+            nulabilniDuplić=null;
+            Console.WriteLine(nulabilniDuplić);
 
-            // TODO:051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
 
-            // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
-
-            // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            
+            try { običniDuplić = nulabilniDuplić.Value; }catch(Exception ex) { Console.WriteLine(ex); }
+            
+            nulabilniDuplić = 5;
+            try { običniDuplić = nulabilniDuplić.Value; } catch (Exception ex) { Console.WriteLine(ex); }
+            Console.WriteLine(običniDuplić);
+            
 
             Console.ReadKey();
         }
