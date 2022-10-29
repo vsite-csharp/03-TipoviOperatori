39fabb46b914761294a80190d55e33294f3f0ff5
Giancarlo <giancarlo.bedek@hotmail.com>
2022-10-29 12:10:00 +0200

    inicijalizacija tipova

--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -6,15 +6,21 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            // Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            int a = new int();
+            Console.WriteLine(a);
+            // Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            //int b = new int(4);
 
-            // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            // Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            //string s = new string();
 
-            // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            // Provjeriti koji su mogući konstruktori za string.
+            string s = new string('a', 10);
+            Console.WriteLine(s);
 
-            // TODO:013 Provjeriti koji su mogući konstruktori za string.
-
-            // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            // Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            string s1 = "string 1";
 
             Console.ReadKey();
         }
