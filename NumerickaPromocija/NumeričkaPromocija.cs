ed482529a356de75497de95ebf16a9e2a0654ac9
Marko Bohač <mabohac@vsite.hr>
2022-11-02 23:27:10 +0100

    Resolved numeric promotion

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -2,21 +2,33 @@
 
 namespace Vsite.CSharp.TipoviOperatori
 {
-    class Program
-    {
-        static void Main()
-        {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
-
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
-
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
-
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+	class Program
+	{
+		static void Main()
+		{
+			// 040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+			int intBroj1 = 3, intBroj2 = 2;
+			Console.WriteLine("{0} / {1} = {2}\t- {3}", intBroj1, intBroj2, (intBroj1 / intBroj2), (intBroj1 / intBroj2).GetType());
+			
+			// 041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+			Console.WriteLine("{0} / {1} = {2}\t- {3}", intBroj1, intBroj2, ((double)intBroj1 / intBroj2), ((double)intBroj1 / intBroj2).GetType());
+			
+			// 042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+			int maxI = int.MaxValue;
+			long longOne = 1;
+			Console.WriteLine("maxI\t= {0}\nlongOne\t= {1}", maxI, longOne);
+			Console.WriteLine((maxI + longOne) + "\t- " + (maxI + longOne).GetType());
+            
+			// 043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+			double dvanaest = 12.0;
+			Console.WriteLine((dvanaest + maxI) + "\t- " + (dvanaest + maxI).GetType());
+            
+			// 044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+			decimal dec = 10.1M;
+            //Console.WriteLine((dvanaest + dec) + "\t- " + (dvanaest + dec).GetType());    //operator error
 
             Console.ReadKey();
+			
         }
     }
 }
