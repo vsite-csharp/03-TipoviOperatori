2d15ae6f4a40892426bf8de51b65ec6247e4956b
Fran Štambuk <fran.stambuk@gmail.com>
2022-11-03 19:31:33 +0100

    dynamic variable

--- DeklaracijaDynamic/DeklaracijaDynamic.cs
+++ DeklaracijaDynamic/DeklaracijaDynamic.cs
@@ -16,15 +16,17 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
-            // TODO:072 Dodati naredbe za ispis polja a i b u toj strukturi.
-
-            // TODO:073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
-
-            // TODO:074 Pokrenuti program i provjeriti ispis.
+            // Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            dynamic s = new S();
+            // Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            s.a = 314;
+            s.b = 2; // "Pero";
+            // Dodati naredbe za ispis polja a i b u toj strukturi.
+            Console.WriteLine(s.a);
+            Console.WriteLine(s.b);
+            // Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            //s.c = 3;
+            // Pokrenuti program i provjeriti ispis.
 
             Console.ReadKey();
         }
