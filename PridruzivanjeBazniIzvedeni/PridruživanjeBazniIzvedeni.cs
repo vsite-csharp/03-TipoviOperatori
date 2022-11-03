cce631c2809284a035636f03ee9c2ad3148d1519
Marko Bohač <mabohac@vsite.hr>
2022-11-03 19:44:16 +0100

    Resolved derived to base class assignment

--- PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
+++ PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
@@ -2,38 +2,41 @@
 
 namespace Vsite.CSharp.TipoviOperatori
 {
-    class Osoba
-    {
-        public void DajOib()
-        {
-            Console.WriteLine("0123456789");
-        }
-    }
+	class Osoba
+	{
+		public void DajOib()
+		{
+			Console.WriteLine("0123456789");
+		}
+	}
 
-    class Student : Osoba
-    {
-        public void PoložiIspit(string predmet)
-        {
-            Console.WriteLine($"Ocjena iz predmeta {predmet} je 3");
-        }
-    }
+	class Student : Osoba
+	{
+		public void PoložiIspit(string predmet)
+		{
+			Console.WriteLine($"Ocjena iz predmeta {predmet} je 5");
+		}
+	}
 
-    class Program
-    {
-        static void Main()
-        {
-            // TODO:080 Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
-            Osoba osoba = new Osoba();
+	class Program
+	{
+		static void Main()
+		{
+			// 080 Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
+			Osoba osoba = new Osoba();
+			osoba.DajOib();
+			Student student = new Student();
+			student.PoložiIspit("c#");
+			student.DajOib();
 
-            Student student = new Student();
+			// 081 Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
+			Osoba osobaStudent = new Student();
+			osobaStudent.DajOib();
 
-            // TODO:081 Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
-            Osoba osobaStudent = new Student();
+			// 082 Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
+			//student = new Osoba();
 
-            // TODO:082 Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
-
-
-            Console.ReadKey();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 }
