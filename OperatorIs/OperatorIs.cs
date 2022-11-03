f8fe0f07ca2b0f5da19507b3d842fd02e2c71066
Marko Bohač <mabohac@vsite.hr>
2022-11-03 20:00:17 +0100

    Resolved operator is

--- OperatorIs/OperatorIs.cs
+++ OperatorIs/OperatorIs.cs
@@ -2,23 +2,25 @@
 
 namespace Vsite.CSharp.TipoviOperatori
 {
-    class Osoba { }
+	class Osoba { }
 
-    class Student : Osoba { }
+	class Student : Osoba { }
 
-    class Program
-    {
-        static void Main()
-        {
-            // TODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
-            Osoba osoba = new Osoba();
+	class Program
+	{
+		static void Main()
+		{
+			// 100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
+			Osoba osoba = new Osoba();
+			Console.WriteLine(osoba is Osoba);
+			Console.WriteLine(osoba is Student);
 
-
-            // TODO:101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
-            Osoba janko = new Student();
-
+			// 101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
+			Osoba janko = new Student();
+            Console.WriteLine(janko is Osoba);
+            Console.WriteLine(janko is Student);
 
             Console.ReadKey();
-        }
-    }
+		}
+	}
 }
