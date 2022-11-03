281dca9de67ce9905dcc5cc6ec909ce603e78990
Giancarlo <giancarlo.bedek@hotmail.com>
2022-11-03 20:00:34 +0100

    Resolve operator is

--- OperatorIs/OperatorIs.cs
+++ OperatorIs/OperatorIs.cs
@@ -10,12 +10,16 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
+            // Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba osoba = new Osoba();
+            Console.WriteLine(osoba is Osoba);
+            Console.WriteLine(osoba is Student);
 
 
-            // TODO:101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
+            // Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba janko = new Student();
+            Console.WriteLine(janko is Osoba);
+            Console.WriteLine(janko is Student);
 
 
             Console.ReadKey();
