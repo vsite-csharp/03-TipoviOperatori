24ab789e7ab691bccaf23bde2e2f9e53b364181e
Gabriel Marvin <GabrielM@dspace.hr>
2022-11-03 20:07:40 +0100

    Resolved Operator As

--- OperatorIs/OperatorIs.cs
+++ OperatorIs/OperatorIs.cs
@@ -12,11 +12,12 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             // TODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba osoba = new Osoba();
-
-
+            Console.WriteLine(osoba is Osoba);
+            Console.WriteLine(osoba is Student);
             // TODO:101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba janko = new Student();
-
+            Console.WriteLine(osoba is Osoba);
+            Console.WriteLine(osoba is Student);
 
             Console.ReadKey();
         }
