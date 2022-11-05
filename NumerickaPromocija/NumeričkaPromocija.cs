182f1859d57d8f19aab8537061dfb7cd4a46d9ca
Giancarlo <giancarlo.bedek@hotmail.com>
2022-11-05 20:55:48 +0100

    Resolve numeric promotion

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -6,15 +6,33 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            // Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int num1 = 4;
+            int num2 = 2;
+            Console.WriteLine((num1 / num2).GetType());
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            // Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            num2 = 3;
+            Console.WriteLine((num1 / num2).GetType());
 
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            // Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int num3 = int.MaxValue;
+            long num4 = 1;
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+            Console.WriteLine(num3 + num4);
 
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            // Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double a = 2.2;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine((a + b).GetType());
 
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            // Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal c = 4;
+            double d = 2;
+            Console.WriteLine(c + (decimal)d); // Operator "+" nije dozvoljen izmedu tipa decimal i double
+            Console.WriteLine((c + (decimal)d).GetType());
 
             Console.ReadKey();
         }
