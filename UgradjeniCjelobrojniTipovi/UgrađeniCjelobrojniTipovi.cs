b4d51c9202733152fcad7a4597546fea7078e7a6
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-26 21:16:11 +0200

    - Inspect integer types

--- UgradjeniCjelobrojniTipovi/UgrađeniCjelobrojniTipovi.cs
+++ UgradjeniCjelobrojniTipovi/UgrađeniCjelobrojniTipovi.cs
@@ -6,28 +6,41 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:020 Otkomentirati donje naredbe koje ispisuju najveće brojeve koji se mogu pohraniti u tip int odn. long.
-            // Console.WriteLine(int.MaxValue);
-            // Console.WriteLine(long.MaxValue);
+            // :020 Otkomentirati donje naredbe koje ispisuju najveće brojeve koji se mogu pohraniti u tip int odn. long.
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(long.MaxValue);
 
-            // TODO:021 Dodati naredbe koje će ispisati najmanje brojeve koji se mogu pohraniti u tip int odn. u tip long.
+            // :021 Dodati naredbe koje će ispisati najmanje brojeve koji se mogu pohraniti u tip int odn. u tip long.
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MinValue);
 
             // Primjeri pridruživanja cjelobrojnih varijabli različitih tipova
-            // TODO:022 Deklarirati varijablu tipa int i dodijeliti joj vrijednost 1. 
-
-            // TODO:023 Deklarirati varijablu tipa long i pridružiti joj vrijednost gornje varijable tipa int.
-
-            // TODO:024 Deklarirati varijablu tipa short i pridružiti joj vrijednost gornje varijable tipa int. 
-
+            // :022 Deklarirati varijablu tipa int i dodijeliti joj vrijednost 1. 
+            int jedan = 1;
+            // :023 Deklarirati varijablu tipa long i pridružiti joj vrijednost gornje varijable tipa int.
+            long jedanLong = jedan;
+            // :024 Deklarirati varijablu tipa short i pridružiti joj vrijednost gornje varijable tipa int. 
+            short jedanShort = (short)jedan;
             // Primjeri numeričkog preljeva
-            // TODO:025 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
-
-            // TODO:026 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
-
-            // TODO:027 U postavkama projekta uključiti opciju za provjeru numeričkog preljeva. Ponovno pokrenuti program i provjeriti što će se dogoditi.
+            // :025 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MaxValue. Ispisati vrijednost varijable uvećane za 1.
+            int maxPreko = int.MaxValue;
+            Console.WriteLine(maxPreko + 1);
+            // :026 Deklarirati varijablu tipa int i dodijeliti joj vrijednost int.MinValue. Ispisati vrijednost varijable umanjenu za 1.
+            int minIspod = int.MinValue;
+            Console.WriteLine(minIspod - 1);
+            // :027 U postavkama projekta uključiti opciju za provjeru numeričkog preljeva. Ponovno pokrenuti program i provjeriti što će se dogoditi.
+            //Greska se generira, dešava se preljev
 
             // Primjer cjelobrojnog dijeljenja nulom
-            // TODO:028 Definirati varijablu tipa int te dodati naredbu kojom se ta varijabla dijeli s nulom. Pokrenuti program i provjeriti što će se dogoditi.
+            // :028 Definirati varijablu tipa int te dodati naredbu kojom se ta varijabla dijeli s nulom. Pokrenuti program i provjeriti što će se dogoditi.
+            try
+            {
+                int nekiBroj = 123;
+                Console.WriteLine(nekiBroj / 0);
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex);
+            }
 
             Console.ReadKey();
         }
--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -6,15 +6,18 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
-
-            // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
-
-            // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
-
-            // TODO:013 Provjeriti koji su mogući konstruktori za string.
-
-            // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            // :010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            int someInt = new int();
+            Console.WriteLine(someInt);
+            // :011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            //int someInt2 = new int(4);
+            // :012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            //string tekst = new string();
+            string tekst = new string(string.Empty);
+            // :013 Provjeriti koji su mogući konstruktori za string.
+            string konstruktor = new string('x', 5);
+            // :014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            string noviString = new string("string 1");
 
             Console.ReadKey();
         }
