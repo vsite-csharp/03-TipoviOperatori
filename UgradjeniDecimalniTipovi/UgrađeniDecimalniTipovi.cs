35908a19ab5f903f329f77ea560b6f44c68a152a
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-26 21:33:18 +0200

    - update to Decimal types

--- UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
+++ UgradjeniDecimalniTipovi/UgrađeniDecimalniTipovi.cs
@@ -23,8 +23,8 @@ namespace Vsite.CSharp.TipoviOperatori
 
             // Primjer binarnog zaokruživanja na rezultat:
             // :033 Definirati dvije varijable: trećina1 i trećina2, tipa double s vrijednostima koje se izračunavaju pomoću izraza 1/3, odn. (1-2/3). Voditi računa da dijeljenje u tim izrazima ne bude cjelobrojno!
-            double trećina1 = 1 / 3;
-            double trećina2 = (1 - 2 / 3);
+            double trećina1 = 1.0 / 3;
+            double trećina2 = (1 - 2.0 / 3);
             // :034 Ispisati vrijednosti varijabli trećina1 i trećina2 te njihove razlike (trećina1-trećina1), odn. (trećina1-tećina2).
             Console.WriteLine(trećina1);
             Console.WriteLine(trećina2);
@@ -40,7 +40,7 @@ namespace Vsite.CSharp.TipoviOperatori
             // :036 Provjeriti što će se ispisati kao rezultat dijeljenja pozitivnog broja tipa double s 0 te dijeljenja negativnog broja tipa double s 0.
             try
             {
-                double nekiBroj = 21355;
+                double nekiBroj = 21355.0;
                 Console.WriteLine(nekiBroj / 0);
                 Console.WriteLine(-nekiBroj / 0);
             }
@@ -52,7 +52,7 @@ namespace Vsite.CSharp.TipoviOperatori
             double nula = 0;
             Console.WriteLine(nula / 0); //NaN
             // :038 Definirati varijablu tipa double i pridružiti joj vrijednost 5.
-            double petica = 5;
+            double petica = 5.0;
             // :039 Varijabli tipa int pridružiti vrijednost prethodno definirane varijable i ispisati njenu vrijednost.
             int brojPet = (int)petica;
             Console.WriteLine(brojPet);
