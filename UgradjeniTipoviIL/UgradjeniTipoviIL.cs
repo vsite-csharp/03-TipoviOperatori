1609a9ca0900fba8bd27b5f7ff5143d681f2539b
Mario Knežević <maknezevic@vsite.hr>
2023-10-26 20:59:23 +0200

    Lookup JLDasm code

--- UgradjeniTipoviIL/UgradjeniTipoviIL.cs
+++ UgradjeniTipoviIL/UgradjeniTipoviIL.cs
@@ -10,7 +10,7 @@ namespace Vsite.CSharp.TipoviOperatori
             // Pogleda li se generirani međukod (IL), npr. pomoću ILDasm-a vidi se da svi tipovi osim 
             // decimal imaju direktne operacije u međujeziku!
             int i = 5;
-            long l = 10;
+            long l = 100;
             l = 4294967296;
             float f = 10.3F;
             double dbl = 12.8;
