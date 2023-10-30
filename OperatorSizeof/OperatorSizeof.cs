61e70359f24a088f223e1a983070a3690d9c55f9
Kristian Mesić <kristian.mesic@king-ict.hr>
2023-10-30 20:35:54 +0100

    - check sizeof operator

--- OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof.cs
@@ -35,14 +35,25 @@ namespace Vsite.CSharp.TipoviOperatori
         static void Main()
         {
             // TODO:120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
-
+            Console.WriteLine(sizeof(decimal));
             // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
-
+            Console.WriteLine(sizeof(double));
             // TODO:122 Ispisati duljinu tipa MojaStruktura
-
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaStruktura));
+            }
             // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
+            }
 
             // TODO:124 Ispisati duljinu tipa MojaKlasa
+            unsafe
+            {
+                Console.WriteLine(sizeof(MojaKlasa));
+            }
 
 
             Console.ReadKey();
