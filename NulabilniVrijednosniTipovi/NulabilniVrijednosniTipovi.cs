536fec7dd0df9349e6ff4ae53889092a5f36d7fa
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-30 07:28:11 +0100

    - check nullable types

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -13,12 +13,14 @@ namespace Vsite.CSharp.TipoviOperatori
             Console.WriteLine(običniDuplić);
 
             // TODO:050  Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
-
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
             // TODO:051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
-
+            običniDuplić = (double)nulabilniDuplić; // mora biti explicitna pretvorba zbog znaka ?
             // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
-
+            nulabilniDuplić = double.MaxValue;
             // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+            običniDuplić = (double)nulabilniDuplić;
 
             Console.ReadKey();
         }
