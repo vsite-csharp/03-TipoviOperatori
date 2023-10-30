be84f118794a0c534d1926ff02a63486a9906e2d
Kristian Mesić <kristian.mesic@king-ict.hr>
2023-10-30 20:19:30 +0100

    - check operator Is

--- OperatorIs/OperatorIs.cs
+++ OperatorIs/OperatorIs.cs
@@ -10,12 +10,20 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
+            // :100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba osoba = new Osoba();
+            Console.WriteLine("Osoba");
+            Console.WriteLine(osoba.GetType());
+            Console.WriteLine(osoba is Osoba); //true
+            Console.WriteLine(osoba is Student); //false
 
 
-            // TODO:101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
+            // :101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba janko = new Student();
+            Console.WriteLine("Janko");
+            Console.WriteLine(janko.GetType());
+            Console.WriteLine(janko is Osoba); //true
+            Console.WriteLine(janko is Student); //true
 
 
             Console.ReadKey();
