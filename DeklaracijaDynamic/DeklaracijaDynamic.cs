d8d78187e8e6f79353bd54eb256d275692f473d7
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-30 07:40:05 +0100

    - check dynamic keyword

--- DeklaracijaDynamic/DeklaracijaDynamic.cs
+++ DeklaracijaDynamic/DeklaracijaDynamic.cs
@@ -16,16 +16,18 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
-
-            // TODO:071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
-            // TODO:072 Dodati naredbe za ispis polja a i b u toj strukturi.
-
-            // TODO:073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
-
-            // TODO:074 Pokrenuti program i provjeriti ispis.
-
+            // :070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            dynamic s = new S();
+            // :071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            s.a = 10;
+            s.b = 20.2;
+            // :072 Dodati naredbe za ispis polja a i b u toj strukturi.
+            Console.WriteLine(s.a);
+            Console.WriteLine(s.b);
+            // :073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            //s.c = 22;
+            // :074 Pokrenuti program i provjeriti ispis.
+            Console.WriteLine(s);
             Console.ReadKey();
         }
     }
