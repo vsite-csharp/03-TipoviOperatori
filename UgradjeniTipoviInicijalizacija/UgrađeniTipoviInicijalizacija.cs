bcf3306c672d7fc80e3428d8e95a66b43711aa7e
Patrik Kralj <patrikkralj97@gmail.com>
2023-10-31 21:01:49 +0100

    Tried integral types init

--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -6,16 +6,21 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            // 010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
 
-            // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            int a;
+            //Console.Write(a);
+            // :011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
+            int b = 4;
+            Console.Write(b);
+            // :012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
 
-            // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
+            string ab;
+            //Console.Write(ab);
+            // :013 Provjeriti koji su mogući konstruktori za string.
 
-            // TODO:013 Provjeriti koji su mogući konstruktori za string.
-
-            // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
-
+            // :014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
+            string abdc = "string 1";
             Console.ReadKey();
         }
     }
