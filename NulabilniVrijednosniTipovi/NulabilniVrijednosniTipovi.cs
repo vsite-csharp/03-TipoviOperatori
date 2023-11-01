5fb2199c6bcf4a638fe65426a3ca52c21e3513d5
Julijan Šribar <julijan.sribar@gmail.com>
2023-11-01 22:44:23 +0100

    Add example for nullable reference type

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -2,6 +2,11 @@
 
 namespace Vsite.CSharp.TipoviOperatori
 {
+    class MojaKlasa
+    {
+        public int a = 10;
+    }
+
     static class Program
     {
         static void Main()
@@ -19,6 +24,18 @@ namespace Vsite.CSharp.TipoviOperatori
             // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
 
             // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
+
+            
+            // TODO:054 Otkomentirati donje naredbe, provjeriti prevodi li se kod, pogledati upozorenja te pokrenuti program i provjeriti ispis.
+            // TODO:055 Deklarirati mk da je nulabilni tip te ponoviti gornje korake.
+            // TODO:056 Ispred drugog poziva za ispis dodati provjeru je li mk inicijaliziran te ispis napraviti samo u tom slučaju.
+            //MojaKlasa? mk = new MojaKlasa();
+
+            //Console.WriteLine(mk.a);
+
+            //mk = null;
+
+            //Console.WriteLine(mk.a);
 
             Console.ReadKey();
         }
