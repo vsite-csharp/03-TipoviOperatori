788a6dbfb5e96b0bdf23b14b439920ecc041a3fc
Tin Bašić <tin.basic22@gmail.com>
2023-11-01 19:00:48 +0100

    Variable declararation part1

--- DeklaracijaVar/DeklaracijaVar.cs
+++ DeklaracijaVar/DeklaracijaVar.cs
@@ -14,17 +14,29 @@ namespace Vsite.CSharp.TipoviOperatori
         {
             // TODO:060 Definirati varijablu "i0" tipa var i inicijalizirati ju nekim cijelim brojem.
 
+            var i0 = 1;
+
             // TODO:061 Definirati varijablu "d0" tipa var i inicijalizirati ju nekim decimalnim brojem.
+            var d0 = 2.0;
 
             // TODO:062 Dodati naredbu: i0 = d0; te provjeriti prihvaća li prevoditelj tu naredbu.
 
+            i0 = d0;
+
             // TODO:063 Definirati varijablu "i1" tipa int i inicijalizirati ju istim cijelim brojem kao varijablu i0.
 
+            var i1 = 1;
+
             // TODO:064 Definirati varijablu "d1" tipa double i inicijalizirati ju istim decimalnim brojem kao varijablu d0.
+
+            double d1 = 2.0;
 
             // TODO:065 Prevesti kod i usporediti kod naredbi za inicijalizacije varijabli i0 i d0, odnosno i1 i d1 pomoću ILDasm-a.
 
             // TODO:066 Definirati varijablu "s" tipa var i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+
+            var s;
+            S s= new S();
 
             // TODO:067 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
 
