4ba948f018a28786dafff6af163e66fbcf5bd552
AnMal <junkie_@msn.com>
2023-11-02 20:13:25 +0100

    Resolved declaration dynamic

--- DeklaracijaDynamic/DeklaracijaDynamic.cs
+++ DeklaracijaDynamic/DeklaracijaDynamic.cs
@@ -10,16 +10,24 @@
     {
         static void Main()
         {
-            // TODO:070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            // :070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            dynamic s = new S();
 
-            // TODO:071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            // :071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            s.a = 54;
+            s.b = 234;
 
-            // TODO:072 Dodati naredbe za ispis polja a i b u toj strukturi.
+            // :072 Dodati naredbe za ispis polja a i b u toj strukturi.
+            Console.WriteLine(s.a);
+            Console.WriteLine(s.b);
 
-            // TODO:073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            // :073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            s.c = "Pero";
+            Console.WriteLine(s.c);
 
-            // TODO:074 Pokrenuti program i provjeriti ispis.
+            // :074 Pokrenuti program i provjeriti ispis.
 
+            Console.WriteLine("GOTOVO");
             Console.ReadKey();
         }
     }
