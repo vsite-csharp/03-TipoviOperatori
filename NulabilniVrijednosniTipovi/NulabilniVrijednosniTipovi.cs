dc834ed73935aabdfce0a0d6b2c657ff1231aca9
Hrvoje Janđel <hrjandje@vsite.hr>
2023-11-02 19:57:17 +0100

    Resolve nullable types

--- NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
+++ NulabilniVrijednosniTipovi/NulabilniVrijednosniTipovi.cs
@@ -17,25 +17,37 @@ namespace Vsite.CSharp.TipoviOperatori
             double običniDuplić = Math.PI;
             Console.WriteLine(običniDuplić);
 
-            // TODO:050 Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
+            // :050 Dodati naredbu kojom se vrijednost nulabilniDuplić postavlja na null referencu te ispisati nakon toga sadržaj varijable.
+            nulabilniDuplić = null;
+            Console.WriteLine(nulabilniDuplić);
+            // :051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
+            if (nulabilniDuplić.HasValue)
+            {
+            običniDuplić = nulabilniDuplić.Value;
+                Console.WriteLine(običniDuplić);
+            }
 
-            // TODO:051 Pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić. Provjeriti što će se dogoditi.
+            // :052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
+            nulabilniDuplić = 1;
+            Console.WriteLine(nulabilniDuplić);
 
-            // TODO:052 Postaviti vrijednost varijable nulabilniDuplić na konačnu vrijednost te ispisati sadržaj.
+            // :053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
 
-            // TODO:053 Ponovno pokušati pridružiti varijabli običniDuplić vrijednost varijable nulabilniDuplić.
 
+            // :054 Otkomentirati donje naredbe, provjeriti prevodi li se kod, pogledati upozorenja te pokrenuti program i provjeriti ispis.
+            // :055 Deklarirati mk da je nulabilni tip te ponoviti gornje korake.
+            // :056 Ispred drugog poziva za ispis dodati provjeru je li mk inicijaliziran te ispis napraviti samo u tom slučaju.
+            MojaKlasa? mk = new MojaKlasa();
+
+            Console.WriteLine(mk.a);
+
+            mk = null;
+            if(mk is not null)
+           // if(mk != null)
+            {
+            Console.WriteLine(mk.a);
+            }
             
-            // TODO:054 Otkomentirati donje naredbe, provjeriti prevodi li se kod, pogledati upozorenja te pokrenuti program i provjeriti ispis.
-            // TODO:055 Deklarirati mk da je nulabilni tip te ponoviti gornje korake.
-            // TODO:056 Ispred drugog poziva za ispis dodati provjeru je li mk inicijaliziran te ispis napraviti samo u tom slučaju.
-            //MojaKlasa? mk = new MojaKlasa();
-
-            //Console.WriteLine(mk.a);
-
-            //mk = null;
-
-            //Console.WriteLine(mk.a);
 
             Console.ReadKey();
         }
