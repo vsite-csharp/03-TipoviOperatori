6fbcca8dfc99627ae1ad434734388bf6ae274524
Josip Grabušić <jograbus@vsite.hr>
2023-11-02 20:13:37 +0100

    Resolve declaration dynamic

--- DeklaracijaDynamic/DeklaracijaDynamic.cs
+++ DeklaracijaDynamic/DeklaracijaDynamic.cs
@@ -10,16 +10,22 @@
     {
         static void Main()
         {
-            // TODO:070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            // :070 Definirati varijablu "s" tipa dynamic i inicijalizirati ju pozivom konstruktora strukture S definirane gore.
+            dynamic s = new S();
+            // :071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
+            s.a = 54;
+            s.b = 234;
 
-            // TODO:071 Napisati naredbe za postavljanje vrijednosti polja a i b u toj strukturi na proizvoljne vrijednosti i provjeriti da li se kod može prevesti.
-
+            Console.WriteLine(s.a);
+            Console.WriteLine(s.b);
             // TODO:072 Dodati naredbe za ispis polja a i b u toj strukturi.
 
-            // TODO:073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            // :073 Napisati naredbu za postavljanje vrijednosti nepostojećeg polja c u toj strukturi na proizvoljnu vrijednosti i provjeriti da li se kod može prevesti.
+            s.c = "Pero";
+            Console.WriteLine(s.c); 
+            // :074 Pokrenuti program i provjeriti ispis.
 
-            // TODO:074 Pokrenuti program i provjeriti ispis.
-
+            Console.WriteLine ("GOTOVO");
             Console.ReadKey();
         }
     }
