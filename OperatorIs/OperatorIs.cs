9e3db426b5b89b66e592eee33be8b9aa930c4c53
DrHrast <petar.huljek@gmail.com>
2023-11-02 21:01:57 +0100

    Resolve is operator.

--- OperatorIs/OperatorIs.cs
+++ OperatorIs/OperatorIs.cs
@@ -8,12 +8,15 @@
     {
         static void Main()
         {
-            // TODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
+            // ODO:100 Dodajte provjere za objekt osoba je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba osoba = new Osoba();
-
+            Console.WriteLine(osoba is Osoba);
+            Console.WriteLine(osoba is Student);
 
             // TODO:101 Dodajte provjere za objekt janko je li tipa Osoba i tipa Student te ispišite rezultate.
             Osoba janko = new Student();
+            Console.WriteLine(janko is Osoba);
+            Console.WriteLine(janko is Student);
 
 
             Console.ReadKey();
