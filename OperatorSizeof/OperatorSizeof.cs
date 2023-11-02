d56430a07b7319edc974c6ba7fd9042326e32c5c
Sonja Perković <soperkov@vsite.hr>
2023-11-02 21:16:45 +0100

    Resolve sizeof operator

--- OperatorSizeof/OperatorSizeof.cs
+++ OperatorSizeof/OperatorSizeof.cs
@@ -32,16 +32,22 @@
     {
         static void Main()
         {
-            // TODO:120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
+            // 120 Napisati naredbu kojom će se ispisati duljina podatka tipa decimal 
+            Console.WriteLine(sizeof(decimal));
+            // 121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
+            Console.WriteLine(sizeof(double));
+            unsafe
+            {
+                // 122 Ispisati duljinu tipa MojaStruktura
+                Console.WriteLine(sizeof(MojaStruktura));
 
-            // TODO:121 Napisati naredbu kojom će se ispisati duljina podatka tipa double 
+                // 123 Ispisati duljinu tipa MojaSloženaStruktura
+                Console.WriteLine(sizeof(MojaSloženaStruktura));
 
-            // TODO:122 Ispisati duljinu tipa MojaStruktura
+                // 124 Ispisati duljinu tipa MojaKlasa
+                Console.WriteLine(sizeof(MojaKlasa));
 
-            // TODO:123 Ispisati duljinu tipa MojaSloženaStruktura
-
-            // TODO:124 Ispisati duljinu tipa MojaKlasa
-
+            }
 
             Console.ReadKey();
         }
