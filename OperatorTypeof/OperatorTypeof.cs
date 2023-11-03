822cc7802b368f1ef8d09702aaa7205b409f5616
Patrik Kralj <patrikkralj97@gmail.com>
2023-11-03 11:26:47 +0100

    Resolved typeof operator

--- OperatorTypeof/OperatorTypeof.cs
+++ OperatorTypeof/OperatorTypeof.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace Vsite.CSharp.TipoviOperatori
 {
@@ -10,7 +11,7 @@ namespace Vsite.CSharp.TipoviOperatori
 
             Console.WriteLine($"*** Svi članovi klase {t.Name} ***");
 
-            // TODO:130 Pokrenuti program i pogledati što će ispisati donja petlja.
+            // :130 Pokrenuti program i pogledati što će ispisati donja petlja.
             foreach (MemberInfo mi in t.GetMembers())
             {
                 Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
@@ -18,15 +19,54 @@ namespace Vsite.CSharp.TipoviOperatori
 
             Console.WriteLine($"*** Sve metode klase {t.Name} ***");
 
-            // TODO:131 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
+            // :131 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
+            foreach(var method in t.GetMethods())
+            {
+                Console.WriteLine($"{method.Name} {method.ReturnType}");
+            }
+            // :132 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+            foreach (var method in t.GetMethods())
+            {
+                Console.WriteLine($"{method.Name} {method.ReturnType}");
+                foreach (var parameters in method.GetParameters())
+                {
+                    Console.WriteLine($"{parameters.Name} {parameters.ParameterType}");
+                }
+            }
+            // :133 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
+            foreach (var method in t.GetMethods())
+            {
+                Console.WriteLine($"{method.Name} {method.ReturnType}");
 
-            // TODO:132 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+                string pravo = "";
+                if (method.IsPrivate)
+                {
+                    pravo = "private";
+                }
+                else if (method.IsFamily)
+                {
+                    pravo = "protected";
+                }
+                else if (method.IsPublic)
+                {
+                    pravo = "public";
+                }
+                Console.WriteLine($"Metoda ima {pravo} pravo pristupa");
 
-            // TODO:133 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
-
-            // TODO:134 Napisati petlju koja će za donju instancu klase Object ispisati imena svih njenih članova.
+                foreach (var parameters in method.GetParameters())
+                {
+                    Console.WriteLine($"{parameters.Name} {parameters.ParameterType}");
+                }
+                
+            }
+            // :134 Napisati petlju koja će za donju instancu klase Object ispisati imena svih njenih članova.
             object o = new object();
             Console.WriteLine($"*** Svi članovi instance klase {o.GetType().Name} ***");
+
+            foreach (var member in o.GetType().GetMembers())
+            {
+                Console.WriteLine($"{member.Name}");
+            }
 
             Console.ReadKey();
         }
