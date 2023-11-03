e8226ee9e78ef91e3cc03238a2044a2aba615ae0
Bogjenero <bornagjenero1@gmail.com>
2023-11-03 10:34:04 +0100

    Homework

--- NumerickaPromocija/NumeričkaPromocija.cs
+++ NumerickaPromocija/NumeričkaPromocija.cs
@@ -4,16 +4,26 @@
     {
         static void Main()
         {
-            // TODO:040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            //040 Definirati dvije varijable tipa int i provjeriti kojeg tipa je njihov kvocijent.
+            int i = 1; int j = 2;
+            Console.WriteLine(i/j); //tipa int 
 
-            // TODO:041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
-
-            // TODO:042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
-
-            // TODO:043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
-
-            // TODO:044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
-
+            // 041 Dijeljitelj i/ili dijeljenik u gornjem izrazu modificirati tako da dijeljenje bude s ostatkom.
+            Console.WriteLine((double)i/j);
+            //042 Definirati varijablu tipa int s vrijednošću int.MaxValue i jednu varijablu tipa long s vrijednošću 1. Ispisati vrijednosti tih varijabli i njihov zbroj.
+            int a = int.MaxValue;
+            long b = 1;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a+b);
+            // 043 Definirati jednu varijablu tipa double, zbrojiti ju s varijablom tipa int i provjeriti kakvog tipa je taj zbroj.
+            double d = 10;
+            int i2 = 11;
+            Console.WriteLine((d+i2).GetType()); // biti će tipa double
+            //044 Definirati varijablu tipa decimal i ispisati zbroj s varijablom tipa double te provjeriti kakvog tipa je taj zbroj.
+            decimal d2 = 1.23m;
+            double d3 = 1.32;
+            //Console.WriteLine((d2+d3).GetType()); biti će greška jer se ne mogu zbrojiti ta dva tipa bez pretvorbe
             Console.ReadKey();
         }
     }
--- OperatorTypeof/OperatorTypeof.cs
+++ OperatorTypeof/OperatorTypeof.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.Intrinsics.X86;
 
 namespace Vsite.CSharp.TipoviOperatori
 {
@@ -10,24 +11,40 @@ namespace Vsite.CSharp.TipoviOperatori
 
             Console.WriteLine($"*** Svi članovi klase {t.Name} ***");
 
-            // TODO:130 Pokrenuti program i pogledati što će ispisati donja petlja.
-            foreach (MemberInfo mi in t.GetMembers())
+            // 130 Pokrenuti program i pogledati što će ispisati donja petlja.
+            /*foreach (MemberInfo mi in t.GetMembers())
             {
                 Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
-            }
+            }*/
 
             Console.WriteLine($"*** Sve metode klase {t.Name} ***");
+          
 
-            // TODO:131 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
+            //131 Dodati petlju koja će za svaki član kojeg vrati metoda GetMethods ispisati ime metode i povratni tip.
+            foreach(MemberInfo mi in t.GetMethods())
+            {
+                Console.WriteLine($"{mi.Name} {mi.MemberType.ToString()}");
+                MethodInfo method = mi  as MethodInfo;
+                ParameterInfo[] parametri = method.GetParameters();
+                foreach(ParameterInfo pi in parametri) 
+                {
+                    Console.WriteLine($"Parametar: {pi.Name}, Tip:{pi.ParameterType}");
+                }
+                if(method.IsPublic) { Console.WriteLine("Public"); }
+                else if (method.IsPrivate) { Console.WriteLine("Private"); }
+                else { Console.WriteLine("Protected");}
+            }
+            //132 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
 
-            // TODO:132 Proširiti tijelo petlje tako da se za svaku metodu ispišu i njeni parametri(tip i ime parametra).
+            //133 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
 
-            // TODO:133 Proširiti tijelo petlje tako da se za svaku metodu ispišu ima li public, protected ili private pravo pristupa.
-
-            // TODO:134 Napisati petlju koja će za donju instancu klase Object ispisati imena svih njenih članova.
+            //134 Napisati petlju koja će za donju instancu klase Object ispisati imena svih njenih članova.
             object o = new object();
             Console.WriteLine($"*** Svi članovi instance klase {o.GetType().Name} ***");
-
+            Type type = o.GetType();
+            MemberInfo[] member = type.GetMembers();
+            foreach(MemberInfo m in member)
+            { Console.WriteLine(member); }
             Console.ReadKey();
         }
     }
