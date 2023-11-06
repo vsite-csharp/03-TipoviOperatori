458d4daf1e5cfcf595b97093d106a061455b03c0
Ivan Fišić <ivfisic@vsite.hr>
2023-11-06 20:12:52 +0100

    Resolve base to derive to class

--- PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
+++ PridruzivanjeBazniIzvedeni/PridruživanjeBazniIzvedeni.cs
@@ -22,16 +22,17 @@ namespace Vsite.CSharp.TipoviOperatori
     {
         static void Main()
         {
-            // TODO:080 Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
+            // 080 Dodajte pozive svih mogućih metoda nad objektima osoba i student te pokrenite program.
             Osoba osoba = new Osoba();
-
+            osoba.DajOib();
             Student student = new Student();
-
-            // TODO:081 Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
+            student.DajOib();
+            student.PoložiIspit("CSH");
+            // 081 Dodajte pozive svih mogućih metoda na objektu osobaStudent te pokrenite program.
             Osoba osobaStudent = new Student();
-
-            // TODO:082 Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
-
+            osobaStudent.DajOib();
+            // 082 Pokušajte objektu student pridružiti novi objekt tipa Osoba. Provjerite što prevoditelj javlja.
+            // Student student1 = new Osoba(); - cannot convert
 
             Console.ReadKey();
         }
